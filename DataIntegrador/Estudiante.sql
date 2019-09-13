5f10c7c16644827630d2896ed5b4f7df7bf496e5
andres_navarrete <andres.navarrete@ucr.ac.cr>
2019-09-13 11:53:40 -0600

    commit inicial de proyecto integrador BASES.INGE II SEMESTRE 2019

---
+++ DataIntegrador/Estudiante.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Estudiante]
+(
+	[EstudianteID]		INT				IDENTITY (1, 1) NOT NULL,
+	[Apellido]			NVARCHAR (50)	NULL,
+	[Nombre]			NVARCHAR (50)	NULL,
+	[FechaMatricula]	DATETIME		NULL,
+	PRIMARY KEY CLUSTERED ([EstudianteID] ASC)
+) 
