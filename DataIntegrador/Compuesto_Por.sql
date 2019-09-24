3bd9de082f0560e9e9c7b45639fa3fc9bc8499a7
Lucho Ramirez <luchoestebanra@hotmail.com>
2019-09-24 10:57:01 -0600

    Add tables Compuesto_Por, Objetivo

--- DataIntegrador/Compuesto_Por.sql
+++ DataIntegrador/Compuesto_Por.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Compuesto_Por]
 (
 	[Codigo_Plan] INT NOT NULL, 
-    [Codigo_Obj] INT NOT NULL,
-	FOREIGN KEY (Codigo_Plan) REFERENCES Objetivo(Codigo)
+    [Codigo_Obj] INT NULL,
+	FOREIGN KEY (Codigo_Obj) REFERENCES Objetivo(Codigo)
 )
