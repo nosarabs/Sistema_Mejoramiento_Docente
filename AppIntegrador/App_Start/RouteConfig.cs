1c5846c2f1c4bd63f034baa8c51e32cb13140a74
JohanC <johan.cordoba@ucr.ac.cr>
2019-09-24 12:03:29 -0600

    Actividad supervisada Historia de usuario id 3

--- AppIntegrador/App_Start/RouteConfig.cs
+++ AppIntegrador/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace AppIntegrador
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "About",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+            );
         }
     }
 }
