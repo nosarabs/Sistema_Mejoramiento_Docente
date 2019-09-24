cfbae3d7af763e2d4954807ab15d37f0d2c85963
Tatiana Briones <tatiana.briones@ucr.ac.cr>
2019-09-24 11:43:41 -0600

    Added function for user logiN

--- DataIntegrador/LoginUsuario.sql
+++ DataIntegrador/LoginUsuario.sql
@@ -3,24 +3,22 @@
 RETURNS BIT
 BEGIN
 
-DECLARE @userID INT, @result BIT
+	DECLARE @userID INT, @result BIT
 
-IF EXISTS (SELECT TOP 1 Username FROM [dbo].[Usuario] WHERE
-Username=@pLoginName)
-BEGIN
+	IF EXISTS (SELECT TOP 1 Username FROM [dbo].[Usuario] WHERE Username=@pLoginName)
+	BEGIN
+		SET @userID=(SELECT Username FROM [dbo].[Usuario] 
+		WHERE Username=@pLoginName AND PasswordHash=HASHBYTES('SHA2_512',
+		@pPassword+CAST(Salt AS NVARCHAR(36))))
 
-SET @userID=(SELECT Username FROM [dbo].[Usuario] WHERE
-Username=@pLoginName AND PasswordHash=HASHBYTES('SHA2_512',
-@pPassword+CAST(Salt AS NVARCHAR(36))))
+		IF(@userID IS NULL)
+			SET @result=0
 
-IF(@userID IS NULL)
-SET @result=0
+		ELSE
+			SET @result=1
+	END
 
-ELSE
-SET @result=1
-END
-
-ELSE
-SET @result=0
-RETURN @result
-end;
+	ELSE
+		SET @result=0
+	RETURN @result
+END;
