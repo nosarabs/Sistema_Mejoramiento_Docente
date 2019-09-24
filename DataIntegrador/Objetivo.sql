111ca4d5ee116ee4f4e34547b23e2ac59d01741e
Lucho Ramirez <luchoestebanra@hotmail.com>
2019-09-24 11:53:48 -0600

    Modified Objetivo

--- DataIntegrador/Objetivo.sql
+++ DataIntegrador/Objetivo.sql
@@ -4,4 +4,5 @@ CREATE TABLE [dbo].[Objetivo]
     [Nombre] NVARCHAR(50) NULL, 
     [Descripcion] NVARCHAR(50) NULL, 
     [Tipo_O] NVARCHAR(20) NULL, 
+	FOREIGN KEY (Tipo_O) REFERENCES Tipo_Objetivo(Nombre)
 )
