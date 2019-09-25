a25ce939b2197bd8a1b6e07ae02212d31fb76086
Marcial Carillo <marcialcarrillov@gmail.com>
2019-09-25 10:16:25 -0600

    testing a different input method, checking DB

--- AppIntegrador/Controllers/UserLoginController.cs
+++ AppIntegrador/Controllers/UserLoginController.cs
@@ -12,17 +12,21 @@ namespace AppIntegrador
 {
     public class UserLoginController : Controller
     {
+
         private DataIntegradorEntities db = new DataIntegradorEntities();
 
         public ActionResult UserLoginView()
         {
+            ViewBag.HTMLCheck = true;
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+
         public ActionResult Login(UserProfile objUser)
         {
+            ViewBag.HTMLCheck = true;
             if (ModelState.IsValid)
             {
                 using (DataIntegradorEntities db = new DataIntegradorEntities())
