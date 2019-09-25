99a30ffede78ae46ad29f16cd98b6fc6e72e32e3
Christian Asch <christian.asch@ucr.ac.cr>
2019-09-25 15:52:30 -0600

    Se agregó el resto de tablas, es necesario agregar las llaves foráneas a las tablas de otros grupos (En particular a Formulario y a Usuario)

---
+++ DataIntegrador/Evaluado_Por.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Evaluado_Por]
+(
+	[CodigoAM] INT NOT NULL, 
+    [CodigoAC] INT NOT NULL,
+	Primary key(CodigoAM, CodigoAC),
+	Foreign key(CodigoAM) References Accion_De_Mejora(Codigo),
+	Foreign key(CodigoAC) references Accionable(Codigo)
+)
