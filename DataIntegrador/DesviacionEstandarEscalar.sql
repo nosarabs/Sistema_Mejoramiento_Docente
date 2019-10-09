6ee764455042862fd8834f2dd953206e0d7f3a26
Daniel Escamilla <Daniel.escamilla@ucr.ac.cr>
2019-10-09 10:57:32 -0600

    Desviacion paso de ser funcion a procedimiento

--- DataIntegrador/DesviacionEstandarEscalar.sql
+++ DataIntegrador/DesviacionEstandarEscalar.sql
@@ -1,23 +1,22 @@
-﻿CREATE FUNCTION [dbo].[DesviacionEstandarEscalar]
+﻿CREATE PROCEDURE [dbo].[DesviacionEstandarEscalar]
 (
 	@FCod CHAR(8),
 	@CSigla VARCHAR(50),
 	@Grupo TINYINT,
 	@GAnno INT,
 	@GSem TINYINT,
-	@PCod CHAR(8)
+	@PCod CHAR(8),
+	@Desviacion Float output
 )
-
-RETURNS FLOAT
 AS
 BEGIN
-	DECLARE @DEV AS FLOAT
-	SELECT @DEV = STDEV(CAST(OpcionSeleccionada AS FLOAT)) FROM Opciones_seleccionadas_respuesta_con_opciones
+	SELECT @Desviacion = STDEV(CAST(OpcionSeleccionada AS FLOAT))
+	FROM Opciones_seleccionadas_respuesta_con_opciones
 			 WHERE FCodigo	= @FCod 
 			 AND CSigla		= @CSigla
 			 AND GNumero	= @GRUPO
 			 AND GAnno		= @GAnno
 			 AND GSemestre	= @GSem
 			 AND PCodigo	= @PCod
-	RETURN @DEV
 END
+RETURN 0
