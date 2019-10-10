fe3f6a11698b48bf62099b5ae4bad53b43ba8384
andres_mesen <andres.mesenmunoz@ucr.ac.cr>
2019-10-09 18:30:09 -0600

    Corrección en los gráficos de barra.

--- AppIntegrador/Scripts/Visualizacion/charts.js
+++ AppIntegrador/Scripts/Visualizacion/charts.js
@@ -61,6 +61,7 @@ function drawBarChart(cvs, chartData) {
             scales: {
                 yAxes: [{
                     ticks: {
+                        beginAtZero: true,
                         fontColor: "black",
                         fontSize: 16,
                     }
