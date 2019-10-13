f12bcec79a632d66e141b44a25ade59d1bdaae7e
Bryan Ulate <bryan.ulate@ucr.ac.cr>
2019-10-13 17:06:52 -0600

    Agregados procedimientos almacenados al proyecto de datos para la creación de secciones con preguntas.
    Co-Authored-By:  Gabriel Galvez<gabriel.galvez@ucr.ac.cr>

---
+++ DataIntegrador/Formularios/Procedimientos/AgregarSección.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[AgregarSección]
+	@Codigo char(8),
+	@Nombre nvarchar(250) 
+AS
+	INSERT INTO Seccion
+	VALUES(@Codigo, @Nombre)
+RETURN 0
---
+++ DataIntegrador/Formularios/Procedimientos/AsociarPreguntaConSeccion.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[AsociarPreguntaConSeccion]
+	@CodigoSeccion char(8),
+	@CodigoPregunta char(8),
+	@Orden int
+AS
+	INSERT INTO Seccion_tiene_pregunta
+	VALUES (@CodigoSeccion, @CodigoPregunta, @Orden)
+RETURN 0
