86eb4f6a3848e2884dfded45e0c4a325007a3143
andresnboza <andresnboza92@gmail.com>
2019-10-13 21:55:12 -0600

    trabajando en los links de las tablas

--- AppIntegrador/Models/Metadata/PlanDeMejoraMetadata.cs
+++ AppIntegrador/Models/Metadata/PlanDeMejoraMetadata.cs
@@ -8,22 +8,24 @@ namespace AppIntegrador.Models.Metadata
 {
     public class PlanDeMejoraMetadata
     {
-        [Display(Name = "Código del plan")]
+        [Display(Name = "Código")]
         [Required(ErrorMessage = "Este campo es necesario")]
         [Range(0, int.MaxValue, ErrorMessage = "Este valor debe ser un número entero no negativo")]
         public int codigo { get; set; }
 
-        [Display(Name = "Nombre del plan")]
+        [Display(Name = "Nombre")]
         [DataType(DataType.Text)]
         [MaxLength(30, ErrorMessage = "La longitud máxima de este campo es de 30 caracteres")]
         public string nombre { get; set; }
 
-        [Display(Name = "Fecha de inicio del plan")]
+        [Display(Name = "Fecha Inicio")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public Nullable<System.DateTime> fechaInicio { get; set; }
 
-        [Display(Name = "Fecha final del plan")]
+        [Display(Name = "Fecha Finalización")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public Nullable<System.DateTime> fechaFin { get; set; }
 
     }
