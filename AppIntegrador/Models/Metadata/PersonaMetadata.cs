e5ee0ec3dedd3af86999e6cdfe2c9ac830260b7a
Eric Rios <eric.rios@ucr.ac.cr>
2019-10-19 22:28:25 -0600

    Cambiados mensajes de error en Usuario. TAM-2.2, TAM-2.4.
    Se cambian los mensajes de error, que salían en inglés, con el ErrorMessage de la validación del modelo.
    Se quitan headers que no tiene sentido mostrar en Creación y Edición de usuarios.

--- AppIntegrador/Models/Metadata/PersonaMetadata.cs
+++ AppIntegrador/Models/Metadata/PersonaMetadata.cs
@@ -9,27 +9,31 @@ namespace AppIntegrador.Models.Metadata
     public class PersonaMetadata
     {
         [Display(Name = "Correo Electrónico")]
-        [StringLength(50, ErrorMessage = "El correo no puede exceder los 50 caracteres. ")]
-        [DataType(DataType.EmailAddress)]
+        [StringLength(50, ErrorMessage = "El correo no puede exceder los 50 caracteres.")]
+        [EmailAddress(ErrorMessage = "Dirección de correo inválida.")]
+        [Required(ErrorMessage = "Este campo es requerido.")]
         public string Correo { get; set; }
         [Display(Name = "Correo Alternativo")]
-        [DataType(DataType.EmailAddress)]
-        [StringLength(50, ErrorMessage = "El correo no puede exceder los 50 caracteres. ")]
+        [EmailAddress(ErrorMessage = "Dirección de correo inválida.")]
+        [StringLength(50, ErrorMessage = "El correo no puede exceder los 50 caracteres.")]
         public string CorreoAlt { get; set; }
-        [StringLength(30, ErrorMessage = "La identificación no puede exceder los 30 caracteres. ")]
+        [StringLength(30, ErrorMessage = "La identificación no puede exceder los 30 caracteres.")]
         [Display(Name = "Identificación")]
+        [Required(ErrorMessage = "Este campo es requerido.")]
         public string Identificacion { get; set; }
         [Display(Name = "Primer Nombre")]
-        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres. ")]
+        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres.")]
+        [Required(ErrorMessage = "Este campo es requerido.")]
         public string Nombre1 { get; set; }
         [Display(Name = "Segundo Nombre")]
-        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres. ")]
+        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres.")]
         public string Nombre2 { get; set; }
         [Display(Name = "Primer Apellido")]
-        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres. ")]
+        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres.")]
+        [Required(ErrorMessage = "Este campo es requerido.")]
         public string Apellido1 { get; set; }
         [Display(Name = "Segundo Apellido")]
-        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres. ")]
+        [StringLength(15, ErrorMessage = "Este campo no puede exceder los 15 caracteres.")]
         public string Apellido2 { get; set; }
         [Display(Name = "Tipo de Identificación")]
         public string TipoIdentificacion { get; set; }
