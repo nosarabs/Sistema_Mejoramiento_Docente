afb8f141c857704175465e3236afb01fe56b2d5e
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-10-19 21:55:14 -0600

    Se intenta leer CSV

---
+++ AppIntegrador/Controllers/DatosCSVController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using AppIntegrador.Models;
+
+namespace AppIntegrador.Controllers
+{
+    public class DatosCSVController : Controller
+    {
+        private DataIntegradorEntities db = new DataIntegradorEntities();
+        // GET: DatosCSV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public static List<DatosCSV> ProcesarCSV(string path)
+        {
+            var model = new DatosCSV();
+            return System.IO.File.ReadAllLines(path)
+                .Skip(1)
+                .Where(row => row.Length > 0)
+                .Select(model.ParseRow).ToList();
+        }
+    }
+}
---
+++ AppIntegrador/Models/DatosCSV.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text;
+
+namespace AppIntegrador.Models
+{
+    public class DatosCSV
+    {
+        public string Nombre { get; set; }
+        public string SiglaCurso { get; set; }
+        public int Anno { get; set; }
+        public int Semestre { get; set; }
+        public int Grupo { get; set; }
+        public DatosCSV ParseRow(string row)
+        {
+            var columnas = row.Split(',');
+            return new DatosCSV()
+            {
+                Nombre = columnas[0],
+                SiglaCurso = columnas[1],
+                Anno = int.Parse(columnas[2]),
+                Semestre = int.Parse(columnas[3]),
+                Grupo = int.Parse(columnas[4])
+            };
+        }
+    }
+}
