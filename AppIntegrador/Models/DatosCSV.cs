0affe5b6915d6cb6c0713798a76d601c9d80e5f7
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-10-20 22:11:28 -0600

    Modificación lectura CSV. Librería LINQ to CSV añadida

--- AppIntegrador/Models/DatosCSV.cs
+++ AppIntegrador/Models/DatosCSV.cs
@@ -14,17 +14,5 @@ namespace AppIntegrador.Models
         public int Anno { get; set; }
         public int Semestre { get; set; }
         public int Grupo { get; set; }
-        public DatosCSV ParseRow(string row)
-        {
-            var columnas = row.Split(',');
-            return new DatosCSV()
-            {
-                Nombre = columnas[0],
-                SiglaCurso = columnas[1],
-                Anno = int.Parse(columnas[2]),
-                Semestre = int.Parse(columnas[3]),
-                Grupo = int.Parse(columnas[4])
-            };
-        }
     }
 }
