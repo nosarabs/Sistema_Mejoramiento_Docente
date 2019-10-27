eaa9e98c476396ac31ce96d25c3c6c6e7f699def
Steven Fernández Espinoza <victor.fernandezespinoza@ucr.ac.cr>
2019-10-27 01:21:23 -0600

    RIP-CBX Inicio de la creación de vistas parciales e integración con la vista parcial principal.
    Se migran los componentes necesarios de la pregunta de opción única a una vista parcial.
    Por ahora hay cierta redundancia de código para no romper la página de pregunta de OU original. Se va refactorizando el código viejo para que sea compatible con la nueva forma de renderizar vistas parciales.
    Se le van poniendo nuevos nombres a los estilos para cuando se empiece a implementar la historia RIP-CSS.

--- AppIntegrador/Controllers/PreguntasController.cs
+++ AppIntegrador/Controllers/PreguntasController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using AppIntegrador.Models;
 
 namespace AppIntegrador.Controllers
@@ -10,27 +11,70 @@ namespace AppIntegrador.Controllers
         // GET: Preguntas/Create
         public ActionResult Create()
         {
-            ViewBag.Codigo = new SelectList(db.Pregunta_con_opciones, "Codigo", "TituloCampoObservacion");
-            ViewBag.Codigo = new SelectList(db.Pregunta_con_respuesta_libre, "Codigo", "Codigo");
             return View();
         }
 
-
-        // POST: Preguntas/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Pregunta pregunta)
+        public ActionResult Create(Pregunta pregunta, List<Opciones_de_seleccion> Opciones)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && pregunta.Codigo.Length > 0 && pregunta.Enunciado.Length > 0)
             {
-                db.Pregunta.Add(pregunta);
-                db.SaveChanges();
+                bool validOptions = Opciones != null;
+                if (validOptions)
+                {
+                    validOptions = false;
+                    foreach (Opciones_de_seleccion opcion in Opciones)
+                    {
+                        if (opcion.Texto != null && opcion.Texto.Length > 0)
+                        {
+                            validOptions = true;
+                        }
+                    }
+                }
+
+                if (!validOptions)
+                {
+                    ModelState.AddModelError("", "Una pregunta de selección única necesita al menos una opción");
+                    return View(pregunta);
+                }
+                try
+                {
+                    if (db.AgregarPreguntaConOpcion(pregunta.Codigo, "U", pregunta.Enunciado, pregunta.Pregunta_con_opciones.TituloCampoObservacion) == 0)
+                    {
+                        ModelState.AddModelError("Codigo", "Código ya en uso.");
+                        return View(pregunta);
+                    }
+                }
+                catch (System.Data.SqlClient.SqlException)
+                {
+                    ModelState.AddModelError("Codigo", "Código ya en uso.");
+                    return View(pregunta);
+                }
+
+                foreach (Opciones_de_seleccion opcion in Opciones)
+                {
+                    db.AgregarOpcion(pregunta.Codigo, (byte)opcion.Orden, opcion.Texto);
+                }
+
+                ViewBag.Message = "Exitoso";
                 return View();
             }
 
-            return View(pregunta);
+            return View();
+        }
+
+        // Retorna la vista "parcial" de pregunta con opciones (.cshtml)
+        public ActionResult PreguntaConOpciones()
+        { 
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult OpcionesDeSeleccion(int i)
+        {
+            ViewBag.i = i;
+            return PartialView();
         }
 
         protected override void Dispose(bool disposing)
