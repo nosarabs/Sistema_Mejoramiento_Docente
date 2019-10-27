ce0980aeb8858823bbf1d1a8c559c8d035e50420
Steven Fernández Espinoza <victor.fernandezespinoza@ucr.ac.cr>
2019-10-26 23:25:49 -0600

    RIP-CBX Primera versión de la vista parcial de creación de preguntas.
    Existen dos versiones del combobox: una que se enlaza bien con el modelo y permite agregar las preguntas, pero no se ve tan bien como la otra versión. La otra versión ya tiene todo listo, pero por alguna razón no está enlazándose con el modelo. Se sube la versión que funciona y la que no está comentada.

---
+++ AppIntegrador/Controllers/PreguntasController.cs
@@ -0,0 +1,45 @@
+﻿using System.Web.Mvc;
+using AppIntegrador.Models;
+
+namespace AppIntegrador.Controllers
+{
+    public class PreguntasController : Controller
+    {
+        private DataIntegradorEntities db = new DataIntegradorEntities();
+
+        // GET: Preguntas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Codigo = new SelectList(db.Pregunta_con_opciones, "Codigo", "TituloCampoObservacion");
+            ViewBag.Codigo = new SelectList(db.Pregunta_con_respuesta_libre, "Codigo", "Codigo");
+            return View();
+        }
+
+
+        // POST: Preguntas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Pregunta pregunta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pregunta.Add(pregunta);
+                db.SaveChanges();
+                return View();
+            }
+
+            return View(pregunta);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- AppIntegrador/Models/Pregunta.cs
+++ AppIntegrador/Models/Pregunta.cs
@@ -12,49 +12,47 @@
 namespace AppIntegrador.Models
 {
 
-using System;
-    using System.Collections.Generic;
-    
-public partial class Pregunta
-{
+    using System;
+        using System.Collections.Generic;
+    using System.Web.Mvc;
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-    public Pregunta()
+    public partial class Pregunta
     {
 
-        this.Responde_respuesta_libre = new HashSet<Responde_respuesta_libre>();
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Pregunta()
+        {
 
-        this.Responde_respuesta_con_opciones = new HashSet<Responde_respuesta_con_opciones>();
+            this.Responde_respuesta_libre = new HashSet<Responde_respuesta_libre>();
 
-        this.Seccion_tiene_pregunta = new HashSet<Seccion_tiene_pregunta>();
+            this.Responde_respuesta_con_opciones = new HashSet<Responde_respuesta_con_opciones>();
+
+            this.Seccion_tiene_pregunta = new HashSet<Seccion_tiene_pregunta>();
+
+        }
+
+        public string Codigo { get; set; }
+
+        public string Enunciado { get; set; }
+
+        public string Tipo { get; set; }
+
+        public virtual Pregunta_con_opciones Pregunta_con_opciones { get; set; }
+
+        public virtual Pregunta_con_respuesta_libre Pregunta_con_respuesta_libre { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+
+        public virtual ICollection<Responde_respuesta_libre> Responde_respuesta_libre { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+
+        public virtual ICollection<Responde_respuesta_con_opciones> Responde_respuesta_con_opciones { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+
+        public virtual ICollection<Seccion_tiene_pregunta> Seccion_tiene_pregunta { get; set; }
 
     }
-
-
-    public string Codigo { get; set; }
-
-    public string Enunciado { get; set; }
-
-    public string Tipo { get; set; }
-
-
-
-    public virtual Pregunta_con_opciones Pregunta_con_opciones { get; set; }
-
-    public virtual Pregunta_con_respuesta_libre Pregunta_con_respuesta_libre { get; set; }
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-
-    public virtual ICollection<Responde_respuesta_libre> Responde_respuesta_libre { get; set; }
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-
-    public virtual ICollection<Responde_respuesta_con_opciones> Responde_respuesta_con_opciones { get; set; }
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-
-    public virtual ICollection<Seccion_tiene_pregunta> Seccion_tiene_pregunta { get; set; }
-
-}
 
 }
