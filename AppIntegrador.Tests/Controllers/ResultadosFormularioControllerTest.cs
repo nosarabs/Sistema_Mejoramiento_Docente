a78bdd7f2590f85cec2596b5da95dfaa64662495
Berta Sánchez Jalet <berta.sanchez@ucr.ac.cr>
2019-10-29 11:09:25 -0600

    HU ID: COD-1 Yo como administrador quiero listar las preguntas asociadas a un formulario para poder verlas de forma ordenada. Berta Sánchez Jalet (driver)  y Andres Mesén Muñoz (navigator). Cumple con el criterio de ser "properly tested". Cobertura: 8.3% Cumplimiento: 10

---
+++ AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppIntegrador;
+using AppIntegrador.Controllers;
+
+namespace AppIntegrador.Tests.Controllers
+{
+    [TestClass]
+    public class ResultadosFormularioControllerTest
+    {
+        [TestMethod]
+        public void TestTipoNotNull()
+        {
+            //Arrange
+            ResultadosFormularioController controller = new ResultadosFormularioController();
+            String codFormulario = "00000001";
+
+            //Act
+            String result = controller.GetTipoPregunta(codFormulario);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
