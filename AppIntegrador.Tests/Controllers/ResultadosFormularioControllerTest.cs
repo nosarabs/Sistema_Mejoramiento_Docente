c2ac2d731c26238e50e85e9406036e1d908b4afa
JosueZR <josue.zeledonruiz@ucr.ac.cr>
2019-10-29 11:17:58 -0600

    HU ID: COD-22: Yo como administrador quiero visualizar la desviación estándar para las respuestas de las preguntas de escala numérica. Participantes: Denisse Alfaro - Josué Zeledón Cumple el DOD al ser testeado de forma apropiada, pasando dicha prueba. Porcentaje de cobertura con rspecto al controlador: 1/12 Porcentaje de cumplimiento: 10

--- AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
+++ AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
@@ -7,14 +7,27 @@ using System.Web.Mvc;
 namespace AppIntegrador.Tests.Controllers
 {
     [TestClass]
-    //COD-4: Visualizar el promedio para las respuestas de las preguntas de tipo escalar
+    
     public class ResultadosFormularioControllerTest
     {
+        //COD-4: Visualizar el promedio para las respuestas de las preguntas de tipo escalar
         [TestMethod]
-        public void TestFormularioNotNull() //Comprueba que la vista de formularios no es nula
+        public void TestPromedioFormulario() //Comprueba que el promedio retornado no es nulo
         {
             ResultadosFormularioController controller = new ResultadosFormularioController();
             String resultado = controller.getPromedio("00000001", "CI0128", 1, 2, 2019, "00000001") as String; //Metodo en el controlador y sus respectivos parametros
+            Assert.IsNotNull(resultado); //Comprobacion de que no es null 
+        }
+
+        // COD-22: Visualizar la desviación estándar para las respuestas de las preguntas de escala numérica
+        [TestMethod]
+        public void TestDesviacionFormulario()
+        {
+            // Arrange
+            ResultadosFormularioController controlador = new ResultadosFormularioController();
+            // Act
+            String resultado = controlador.obtenerDesviacionEstandar("00000001", "CI0128", 1, 2, 2019, "00000001") as String; //Metodo en el controlador y sus respectivos parametros
+            // Assert
             Assert.IsNotNull(resultado); //Comprobacion de que no es null 
         }
     }
