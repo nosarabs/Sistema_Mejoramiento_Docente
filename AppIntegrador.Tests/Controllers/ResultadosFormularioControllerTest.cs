3d185e0f528c7ea74b120bddb6fe0514e4ae36fc
andres_mesen <andres.mesenmunoz@ucr.ac.cr>
2019-10-29 11:30:08 -0600

    HU: COD-1 Yo como administador quiero listar las preguntas asociadas a un formulario para poder verlas de forma ordenada. DoD: cumple con el criterio de ser "properly tested". Cobertura: 8.3% Cumplimiento: 10/10

--- AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
+++ AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
@@ -6,12 +6,17 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AppIntegrador;
 using AppIntegrador.Controllers;
+using AppIntegrador.Models;
 
 namespace AppIntegrador.Tests.Controllers
 {
     [TestClass]
     public class ResultadosFormularioControllerTest
     {
+        /*
+        HU: COD-1
+        Tarea técnica: obtener el tipo de la pregunta a partir de un código de formulario existente
+        */
         [TestMethod]
         public void TestTipoNotNull()
         {
@@ -21,6 +26,24 @@ namespace AppIntegrador.Tests.Controllers
 
             //Act
             String result = controller.GetTipoPregunta(codFormulario);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        /*
+        HU: COD-1
+        Tarea técnica: obtener la lista de preguntas asociadas a un código de formulario existente
+        */
+        [TestMethod]
+        public void TestObtenerPreguntaNotNull()
+        {
+            //Arrange
+            ResultadosFormularioController controller = new ResultadosFormularioController();
+            String codigoFormulario = "00000001";
+
+            //Act
+            List<Preguntas> result = controller.ObtenerPreguntas(codigoFormulario);
 
             //Assert
             Assert.IsNotNull(result);
