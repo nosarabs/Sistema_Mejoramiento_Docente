2bb59689111f69b683a54b5db51546b5a6f28c19
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-10-29 11:10:06 -0600

    COD-4: Visualizar el promedio para las respuestas de las preguntas de tipo escalar. Participantes: Denisse Alfaro, Josué Zeledón Cumple el DOD porque fue testeado correctamente Cobertura: 1/12 Cumplimiento: 5

---
+++ AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppIntegrador.Controllers;
+using System.Web.Mvc;
+
+
+namespace AppIntegrador.Tests.Controllers
+{
+    [TestClass]
+    //COD-4: Visualizar el promedio para las respuestas de las preguntas de tipo escalar
+    public class ResultadosFormularioControllerTest
+    {
+        [TestMethod]
+        public void TestFormularioNotNull() //Comprueba que la vista de formularios no es nula
+        {
+            ResultadosFormularioController controller = new ResultadosFormularioController();
+            String resultado = controller.getPromedio("00000001", "CI0128", 1, 2, 2019, "00000001") as String; //Metodo en el controlador y sus respectivos parametros
+            Assert.IsNotNull(resultado); //Comprobacion de que no es null 
+        }
+    }
+}
