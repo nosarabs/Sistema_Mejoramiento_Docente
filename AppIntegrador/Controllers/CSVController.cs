5c999854753c5e7ca886e83e5c3be4d00fd5b8b5
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-11-02 16:52:24 -0600

    COD-68. Tareas técnicas:  Leer el archivo ingresado, Meter los datos en la aplicación

--- AppIntegrador/Controllers/CSVController.cs
+++ AppIntegrador/Controllers/CSVController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LINQtoCSV;
+using System.Data.Entity.Core.Objects;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using AppIntegrador.Models;
 
 namespace AppIntegrador.Controllers
 {
     public class CSVController : Controller
     {
-        // GET: CSV
         public ActionResult Index()
         {
-            return View();
+            CsvFileDescription inputFileDescription = new CsvFileDescription
+            {
+                SeparatorChar = ',', //Indica qué es lo que separa cada valor en el archivo
+                FirstLineHasColumnNames = true //La primera fila corresponde a los títulos de los campos, no a un campo específico
+            };
+            CsvContext cc = new CsvContext();
+            //Este IEnumerable tiene cada modelo que fue llenado con los datos del CSV
+            IEnumerable<ArchivoCSV> datos = cc.Read<ArchivoCSV>("c:\\Users\\Denisse Alfaro\\Documents\\DatosCSV.csv", inputFileDescription); //TODO: De momento el path está fijo
+            List<ArchivoCSV> lista = datos.ToList(); 
+            
+            return View(lista[0]); //TODO: Cambiar esto. Fue usado solo para prueba
         }
     }
 }
---
+++ AppIntegrador/Models/ArchivoCSV.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LINQtoCSV;
+using System.Data;
+using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc;
+using AppIntegrador.Models;
+
+namespace AppIntegrador.Models
+{
+    public class ArchivoCSV
+    {
+        public String CodigoUnidad { get; set; }
+        public String NombreFacultad { get; set; }
+        public String CodigoCarrera { get; set; }
+        public String NombreCarrera { get; set; }
+        public String CodigoEnfasis { get; set; }
+        public String NombreEnfasis { get; set; }
+        public String SiglaCurso { get; set; }
+        public String NombreCurso { get; set; }
+        public int NumeroGrupo { get; set; }
+        public int Anno { get; set; }
+        public int Semestre { get; set; }
+        public String CorreoProfesor { get; set; }
+        public String IdProfesor { get; set; }
+        public String NombreProfesor { get; set; }
+        public String ApellidoProfesor { get; set; }
+        public String TipoIdProfesor { get; set; }
+        public String CorreoEstudiante { get; set; }
+        public String IdEstudiante { get; set; }
+        public String NombreEstudiante { get; set; }
+        public String ApellidoEstudiante { get; set; }
+        public String TipoIdEstudiante { get; set; }
+    }
+}
