816aba42ac059adbeabba52ae2ea60c747e648be
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-11-02 12:40:19 -0600

    COD-68. Librería de lectura y controlador

---
+++ AppIntegrador/Controllers/CSVController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppIntegrador.Controllers
+{
+    public class CSVController : Controller
+    {
+        // GET: CSV
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
