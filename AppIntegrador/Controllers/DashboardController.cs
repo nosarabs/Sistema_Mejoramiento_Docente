befc8af4c938dcef3fbefa732c37e72f9215f689
Ismael_G <ismael.gutierrezhidalgo@ucr.ac.cr>
2019-11-02 23:24:03 -0600

    Historia: COD-61 Yo como administrador quiero filtrar un conjunto de formularios para facilitar la búsqueda.
    Tareas:
    
    - Pequeño cambio en el controlador del dashboard y en el layout para que la vista del dashboard se muestre.

--- AppIntegrador/Controllers/DashboardController.cs
+++ AppIntegrador/Controllers/DashboardController.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using AppIntegrador.Models;
+using System.Data.Entity.Core.Objects;
 
 namespace AppIntegrador.Controllers
 {
     public class DashboardController : Controller
     {
+        private DataIntegradorEntities db = new DataIntegradorEntities();
+
         // GET: Dashboard
         public ActionResult Index()
         {
             return View();
         }
 
+        public List<FiltroFormulariosModel> FiltrarFormularios(String codigoUA, String codigoCarrera, String codigoEnfasis, String siglaCurso, Byte? numeroGrupo, Byte? semestre, Int32? ano, String correoProfesor)
+        {
+            
+        }
     }
 }
