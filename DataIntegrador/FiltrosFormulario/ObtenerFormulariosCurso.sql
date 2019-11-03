b868ba5f581ce95605309d558c9955ccb9bcc945
Ismael_G <ismael.gutierrezhidalgo@ucr.ac.cr>
2019-11-02 20:52:37 -0600

    Historia: COD-61 Yo como administrador quiero filtrar un conjunto de formularios para facilitar la búsqueda.
    Tareas:
    
    - Modificaciones en el PostDeploymentScript para poder probar la funcionalidad de filtrar los formularios de un semestre.
    
    - Se agregaron funciones para poder obtener tablas con información de formularios de un Curso, Grupo (# para ser preciso), Semestre, Año.

---
+++ DataIntegrador/FiltrosFormulario/ObtenerFormulariosCurso.sql
@@ -0,0 +1,39 @@
+﻿/*
+Retorna una tabla con el código de formulario, sigla del curso, número de grupo, semestre, año, fecha de inicio, fecha de finalización de todos los formularios que pertenecen a un curso.
+Esta información se necesita para filtrar respuestas a formulario.
+*/
+CREATE FUNCTION ObtenerFormulariosCurso (@siglaCurso AS VARCHAR(10))
+RETURNS @formulariosCurso TABLE
+(
+	FCodigo CHAR(8),	/*Código del formulario.*/
+	CSigla VARCHAR(10),	/*Sigla del curso.*/
+	GNumero TINYINT,	/*Número de grupo.*/
+	GSemestre TINYINT,	/*Número de semestre.*/
+	GAnno INT,			/*Año.*/
+	FechaInicio DATE,	/*Fecha de inicio del periodo de llenado el formulario.*/
+	FechaFin DATE		/*Fecha de finalización del periodo de llenado del formulario.*/
+)
+AS
+BEGIN
+	
+	/*Si la sigla del curso no es nula, verifica si es válida y recupera la información de los formularios.*/
+	IF (@siglaCurso IS NOT NULL)
+	BEGIN
+
+		/*Si la sigla del curso es válida, recupera la información de los formularios.*/
+		IF (EXISTS (SELECT * FROM Curso WHERE Sigla = @siglaCurso))
+		BEGIN
+
+			INSERT INTO @formulariosCurso
+			SELECT	PAP.FCodigo, PAP.CSigla, PAP.GNumero, PAP.GSemestre, PAP.GAnno, PAP.FechaInicio, PAP.FechaFin
+			FROM	Periodo_activa_por AS PAP
+			WHERE	PAP.FechaFin < CONVERT (DATE, GETDATE()) /*Solo de formularios cuyo periodo de llenado haya finalizado.*/
+					AND PAP.CSigla = @siglaCurso;			 /*Solo de formularios que pertenecen al curso.*/
+
+		END;
+
+	END;
+
+	RETURN;
+
+END;
---
+++ DataIntegrador/FiltrosFormulario/ObtenerFormulariosGrupo.sql
@@ -0,0 +1,39 @@
+﻿/*
+Retorna una tabla con el código de formulario, sigla del curso, número de grupo, semestre, año, fecha de inicio, fecha de finalización de todos los formularios que pertenecen a un grupo con cierto número.
+Esta información se necesita para filtrar respuestas a formulario.
+*/
+CREATE FUNCTION ObtenerFormulariosGrupo (@numeroGrupo AS TINYINT)
+RETURNS @formulariosGrupo TABLE
+(
+	FCodigo CHAR(8),	/*Código del formulario.*/
+	CSigla VARCHAR(10),	/*Sigla del curso.*/
+	GNumero TINYINT,	/*Número de grupo.*/
+	GSemestre TINYINT,	/*Número de semestre.*/
+	GAnno INT,			/*Año.*/
+	FechaInicio DATE,	/*Fecha de inicio del periodo de llenado el formulario.*/
+	FechaFin DATE		/*Fecha de finalización del periodo de llenado del formulario.*/
+)
+AS
+BEGIN
+	
+	/*Si el número de grupo no es nulo, verifica si es válido y recupera la información de los formularios.*/
+	IF (@numeroGrupo IS NOT NULL)
+	BEGIN
+
+		/*Si el número de grupo es válido, recupera la información de los formularios.*/
+		IF (EXISTS (SELECT * FROM Grupo WHERE NumGrupo = @numeroGrupo))
+		BEGIN
+
+			INSERT INTO @formulariosGrupo
+			SELECT	PAP.FCodigo, PAP.CSigla, PAP.GNumero, PAP.GSemestre, PAP.GAnno, PAP.FechaInicio, PAP.FechaFin
+			FROM	Periodo_activa_por AS PAP
+			WHERE	PAP.FechaFin < CONVERT (DATE, GETDATE()) /*Solo de formularios cuyo periodo de llenado haya finalizado.*/
+					AND PAP.GNumero = @numeroGrupo;			 /*Solo de formularios que pertenecen al grupo con el número parámetro.*/
+
+		END;
+
+	END;
+
+	RETURN;
+
+END;
