690d2019dcf185d6e9bc7764477d4a6ac8c4c2f8
Eric Rios <eric.rios@ucr.ac.cr>
2019-11-04 12:36:56 -0600

    Se conecta el método que actualiza los checks con la vista. TAM-3.2.
    Se actualiza el modelo.
    Se corrigen nombres de parámetros para los stored procedures desde el controlador.
    Aún faltan muchas correcciones para obtener los datos correctos de la vista.

--- AppIntegrador/Controllers/PermissionsController.cs
+++ AppIntegrador/Controllers/PermissionsController.cs
@@ -26,15 +26,98 @@ namespace AppIntegrador.Controllers
 
         /* Se llama cuando se quieren guardar los cambios */
         [HttpPost]
-        public ActionResult Index(PermissionsViewHolder Data)
+        public ActionResult Index(PermissionsViewHolder Data, bool updateView)
         {
             if (!PermissionManager.IsAuthorized(PermissionManager.Permission.EDITAR_USUARIOS))
             {
                 TempData["alertmessage"] = "No tiene permisos para acceder a esta página.";
                 return RedirectToAction("../Home/Index");
             }
+            // Actualizar los checkboxes con la selección de énfasis.
+            if (updateView)
+            {
+                ObjectParameter tienePerfil = new ObjectParameter("tienePerfil", typeof(bool));
+                ObjectParameter tieneActivo = new ObjectParameter("tieneActivo", typeof(bool));
+                // Para revisar si el usuario tiene todos esos perfiles
+                int total = 0;
+                int correct = 0;
+
+                // Revisa los checks de las personas
+                foreach (Persona persona in Data.Personas)
+                {
+                    total = 0;
+                    correct = 0;
+                    for (int i = 0; i < Data.PerfilesSeleccionados.Length; ++i)
+                    {
+                        ++total;
+                        // Se asume una sola carrera y un solo énfasis
+                        db.TienePerfilEnElEnfasis(persona.Correo, Data.PerfilesSeleccionados[i], Data.CarrerasSeleccionadas[0], Data.EnfasisSeleccionados[0], tienePerfil);
+
+                        if ((bool)tienePerfil.Value)
+                        {
+                            // Si tiene el perfil asignado, aumente contador
+                            ++correct;
+                        }
+                    }
+
+                    // Tiene al menos un perfil
+                    if (correct > 0)
+                    {
+                        // Tiene todos los perfiles
+                        if (total == correct)
+                        {
+                            persona.HasProfileInEmph = true;
+                        }
+                        // No tiene todos
+                        /*else
+                        {
+                            // TO-DO: Cambiar con Javascript checkbox a "indeterminado"
+                        }*/
+                    }
+                }
+
+                // Revisa los checks de los permisos
+                foreach (Permiso permiso in Data.Permisos)
+                {
+                    total = 0;
+                    correct = 0;
+                    for (int i = 0; i < Data.PerfilesSeleccionados.Length; ++i)
+                    {
+                        ++total;
+                        // Se asume una sola carrera y un solo énfasis
+                        db.TienePermisoActivoEnEnfasis(permiso.Id, Data.PerfilesSeleccionados[i], Data.CarrerasSeleccionadas[0], Data.EnfasisSeleccionados[0], tieneActivo);
+
+                        if ((bool)tieneActivo.Value)
+                        {
+                            // Si está activado en el perfil, aumente contador
+                            ++correct;
+                        }
+                    }
+
+                    // Activo en al menos un perfil
+                    if (correct > 0)
+                    {
+                        // Activo en todos los perfiles
+                        if (total == correct)
+                        {
+                            permiso.ActiveInProfileEmph = true;
+                        }
+                        // No tiene todos
+                        /*else
+                        {
+                            // TO-DO: Cambiar con Javascript checkbox a "indeterminado"
+                        }*/
+                    }
+                }
+            }
             //TO-DO: Guardar aquí la selección de perfil, permisos, usuarios, carrera 
             //y énfasis en la base de datos.
+            
+            // Guardar cambios en la base de datos
+            else
+            {
+
+            }
             return View(Data);
         }
 
