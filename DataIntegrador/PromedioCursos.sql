a576b00a08db0124a4cfab968bbaec81ce4361a7
Berta Sánchez Jalet <berta.sanchez@ucr.ac.cr>
2019-11-05 11:17:35 -0600

    COD-67: Desplegar la información del puntaje de un profesor y un curso específico. Tarea técnica: Realizar consultas a la BD por medio de procedimientos almacenados. Criterios: Mostrar gráficos del promedio total de la calificación de el o los profesores seleccionados en el filtro al igual que el promedio de la calificación de todos los cursos que imparten. DoD: Aún no cumple todos los requisitos, por lo que todavía no se integra a Master. Cobertura: Aún no se han implementado pruebas. Cumplimiento: 2/10

--- DataIntegrador/PromedioCursos.sql
+++ DataIntegrador/PromedioCursos.sql
@@ -1,8 +1,15 @@
-﻿--
+﻿--Berta Sánchez Jalet
+--COD-67: Desplegar la información del puntaje de un profesor y un curso específico.
+--Tarea técnica: Realizar consultas a la BD por medio de procedimientos almacenados.
+--Cumplimiento: 2/10
 
 CREATE PROCEDURE [dbo].[PromedioCursos]
-	@param1 int = 0,
-	@param2 int
+	(@codigoFromulario VARCHAR(8)
+	)
 AS
-	SELECT @param1, @param2
+	BEGIN
+		SELECT
+		FROM
+		WHERE = @codigoFromulario
+	END
 RETURN 0
--- DataIntegrador/PromedioProfesor.sql
+++ DataIntegrador/PromedioProfesor.sql
@@ -1,6 +1,15 @@
-﻿CREATE PROCEDURE [dbo].[PromedioProfesor]
-	@param1 int = 0,
-	@param2 int
+﻿--Berta Sánchez Jalet
+--COD-67: Desplegar la información del puntaje de un profesor y un curso específico.
+--Tarea técnica: Realizar consultas a la BD por medio de procedimientos almacenados.
+--Cumplimiento: 2/10
+
+CREATE PROCEDURE [dbo].[PromedioCursos]
+	(@codigoFromulario VARCHAR(8)
+	)
 AS
-	SELECT @param1, @param2
+	BEGIN
+		SELECT
+		FROM
+		WHERE = @codigoFromulario
+	END
 RETURN 0
