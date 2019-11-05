87b44b03da68495eb546b003ad18a80ed163c853
Tatiana Briones <tatiana.briones@ucr.ac.cr>
2019-11-05 10:45:04 -0600

    TAM 3.7, Cambiar el perfil, la carrera y el énfasis con el que estoy trabajando. TaskId 2, Pruebas Cumple con el DoD Porcentaje de cobertura: %19.4 Cumplimiento: 10

---
+++ AppIntegrador.Tests/Controllers/PermissionsControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using AppIntegrador;
+using AppIntegrador.Controllers;
+using AppIntegrador.Models;
+using System;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppIntegrador.Tests.Controllers
+{
+    /* TAM 3.7 TaskId - 2 Prueba de unidad para cargar perfiles que tiene un usuario*/
+    [TestClass]
+    public class PermissionsControllerTest
+    {
+        [TestMethod]
+        public void CargarPerfil()
+        {
+            PermissionsController controller = new PermissionsController();
+            CurrentUser.Username = "admin@mail.com";
+            JsonResult perfiles = controller.CargarPerfil();
+            string test = "{\"data\":[\"Superusuario\"]}";
+            string result = new JavaScriptSerializer().Serialize(perfiles.Data);
+
+            Assert.AreEqual(test, result);
+        }
+    }
+}
