8cd2e235b9698bf7d1c988edf7427feb193afae0
Daniel.escamilla <daniel.escamilla@ucr.ac.cr>
2019-11-05 11:15:04 -0600

    Pair Programming con Denisse Alfaro
    COD-70: Yo como administrador quiero almacenar los datos de un archivo CSV en el sistema
    Tarea técnica: Cargar datos en blanco con el dato anteriomente registrado
    Ya cumple con el DoD
    10/10
    Ya se probo el método y cumple tiene 64% de cobertura del controlador.
    
    Criterios de aceptación:
    Si entra un archivo inválido no se cargan los datos.
    Permite la lectura de campos en blanco para facilitar la digitación.
    Al terminar la carga se actualiza la base de datos con los valores ingresados.

---
+++ AppIntegrador.Tests/Controllers/CSVcontrollerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+using AppIntegrador.Models;
+using AppIntegrador.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppIntegrador.Tests.Controllers
+{
+    [TestClass]
+    public class CSVcontrollerTest
+    {
+
+
+
+       // COD-70: Yo como administrador quiero almacenar los datos de un archivo CSV en el sistema
+       // Tarea técnica: Cargar datos en blanco con el dato anteriomente registrado
+
+       // Cuando se cree el modulo de cargar archivos CSV se debe modificar el tests
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            CSVController controller = new CSVController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
