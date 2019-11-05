8b60b69f6c741c5e02a68c599033d0a4b28fbfe7
Tatiana Briones <tatiana.briones@ucr.ac.cr>
2019-11-05 01:34:21 -0600

    TAM-3.7-1 Página de configuración de perfil de trabajo para cambiar el perfil, la carrera y el énfasis con el que está trabajando el usuario.
    Falta verificar JQuery.

---
+++ AppIntegrador/Controllers/ConfiguracionUsuarioController.cs
@@ -0,0 +1,34 @@
+﻿using AppIntegrador.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppIntegrador.Controllers
+{
+    public class ConfiguracionUsuarioController : Controller
+    {
+        private DataIntegradorEntities db = new DataIntegradorEntities();
+        // GET: ConfiguracionUsuario
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public JsonResult CargarPerfil()
+        {
+            string username = CurrentUser.Username;
+            List<string> perfiles = new List<string>();
+            using (var context = new DataIntegradorEntities())
+            {
+                var listaPerfiles = from Perfil in db.PerfilesXUsuario(username)
+                                    select Perfil;
+                foreach (var nombrePerfil in listaPerfiles)
+                    perfiles.Add(nombrePerfil.NombrePefil);
+            }
+            return Json(new { data = perfiles }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
