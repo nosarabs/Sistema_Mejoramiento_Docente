44c33df69b9e35ba12db0311516a27494fed624f
Daniel Escamilla <Daniel.escamilla@ucr.ac.cr>
2019-11-06 14:30:16 -0600

    HU: COD-70 Task: Validar entradas
    Condicion para no permitir meter la primera fila vacia

--- AppIntegrador/Controllers/CSVController.cs
+++ AppIntegrador/Controllers/CSVController.cs
@@ -39,14 +39,14 @@ namespace AppIntegrador.Controllers
         {
 
             /*Unidad academica*/
-            if (archivo.CodigoUnidad.Length > 10) //CodigoUnidad
+            if (archivo.CodigoUnidad.Length > 10 && !string.IsNullOrEmpty(archivo.CodigoUnidad)) //CodigoUnidad
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("CodigoUnidad");
 
                 return false;
             }
-            if (archivo.NombreFacultad.Length > 50) //nombreFacultad
+            if (archivo.NombreFacultad.Length > 50 && !string.IsNullOrEmpty(archivo.NombreFacultad)) //nombreFacultad
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("nombreFacultad");
@@ -54,13 +54,13 @@ namespace AppIntegrador.Controllers
             }
 
             /*Carrera*/
-            if (archivo.CodigoCarrera.Length > 10) //CodigoCarrera
+            if (archivo.CodigoCarrera.Length > 10 && !string.IsNullOrEmpty(archivo.CodigoCarrera)) //CodigoCarrera
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("CodigoCarrera");
                 return false;
             }
-            if (archivo.NombreCarrera.Length > 50) //NombreCarrera
+            if (archivo.NombreCarrera.Length > 50 && !string.IsNullOrEmpty(archivo.NombreCarrera)) //NombreCarrera
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("NombreCarrera");
@@ -69,14 +69,14 @@ namespace AppIntegrador.Controllers
             }
 
             /*Enfasis*/
-            if (archivo.CodigoEnfasis.Length > 10) //CodigoEnfasis
+            if (archivo.CodigoEnfasis.Length > 10 && !string.IsNullOrEmpty(archivo.CodigoEnfasis)) //CodigoEnfasis
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("CodigoEnfasis");
 
                 return false;
             }
-            if (archivo.NombreEnfasis.Length > 50) //NombreEnfasis
+            if (archivo.NombreEnfasis.Length > 50 && !string.IsNullOrEmpty(archivo.NombreEnfasis)) //NombreEnfasis
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("NombreEnfasis");
@@ -85,13 +85,13 @@ namespace AppIntegrador.Controllers
             }
 
             /*Curso*/
-            if (archivo.SiglaCurso.Length > 10) //Sigla Curso
+            if (archivo.SiglaCurso.Length > 10 && !string.IsNullOrEmpty(archivo.SiglaCurso)) //Sigla Curso
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("Sigla Curso");
                 return false;
             }
-            if (archivo.NombreCurso.Length > 50) //NombreCurso
+            if (archivo.NombreCurso.Length > 50 && !string.IsNullOrEmpty(archivo.NombreCurso)) //NombreCurso
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("NombreCurso");
@@ -101,7 +101,7 @@ namespace AppIntegrador.Controllers
 
             /*NumGrupo*/
             /*Compara que el valor ingresado sea un numero positivo*/
-            if (!int.TryParse(archivo.NumeroGrupo, out int numGrupo) && numGrupo < 0)  //NumGrupo
+            if (!int.TryParse(archivo.NumeroGrupo, out int numGrupo) && numGrupo < 0 && !string.IsNullOrEmpty(archivo.NumeroGrupo))  //NumGrupo
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("NumGrupo");
@@ -109,7 +109,7 @@ namespace AppIntegrador.Controllers
                 return false;
             }
             /*Compara que el valor ingresado sea un numero positivo*/
-            if (!int.TryParse(archivo.Anno, out int anno) && anno < 0)  //anno
+            if (!int.TryParse(archivo.Anno, out int anno) && anno < 0 && !string.IsNullOrEmpty(archivo.Anno))  //anno
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("anno");
@@ -117,7 +117,7 @@ namespace AppIntegrador.Controllers
                 return false;
             }
             /*Compara que el valor ingresado sea un numero positivo menor o igual a 3*/
-            if (!int.TryParse(archivo.Semestre, out int semestre) && semestre < 0 && semestre > 3)  //semestre
+            if (!int.TryParse(archivo.Semestre, out int semestre) && semestre < 0 && semestre > 3 && !string.IsNullOrEmpty(archivo.Semestre))  //semestre
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("semestre");
@@ -125,34 +125,34 @@ namespace AppIntegrador.Controllers
             }
 
             /*Profesor*/
-            if (archivo.CorreoProfesor.Length > 50) //CorreoDocente
+            if (archivo.CorreoProfesor.Length > 50 && !string.IsNullOrEmpty(archivo.CorreoProfesor)) //CorreoDocente
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("CorreoDocente");
                 return false;
             }
-            if (archivo.IdProfesor.Length > 30) //Id Profesor
+            if (archivo.IdProfesor.Length > 30 && !string.IsNullOrEmpty(archivo.IdProfesor)) //Id Profesor
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("Id Profesor");
 
                 return false;
             }
-            if (archivo.NombreProfesor.Length > 15) //Nombre Profe
+            if (archivo.NombreProfesor.Length > 15 && !string.IsNullOrEmpty(archivo.NombreProfesor)) //Nombre Profe
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("Nombre Profe");
 
                 return false;
             }
-            if (archivo.ApellidoProfesor.Length > 15) //apellidoProfa
+            if (archivo.ApellidoProfesor.Length > 15 && !string.IsNullOrEmpty(archivo.ApellidoProfesor)) //apellidoProfa
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("apellido Profe");
 
                 return false;
             }
-            if (archivo.TipoIdProfesor.Length > 15) //TipoIdProfesor
+            if (archivo.TipoIdProfesor.Length > 15 && !string.IsNullOrEmpty(archivo.TipoIdProfesor)) //TipoIdProfesor
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("TipoIdProfesor");
@@ -161,35 +161,35 @@ namespace AppIntegrador.Controllers
             }
 
             /*Estudiante*/
-            if (archivo.CorreoEstudiante.Length > 50) //CorreoEstudiantes
+            if (archivo.CorreoEstudiante.Length > 50 && !string.IsNullOrEmpty(archivo.CorreoEstudiante)) //CorreoEstudiantes
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("CorreoEstudiantes");
 
                 return false;
             }
-            if (archivo.IdEstudiante.Length > 30) //Id Estudiante
+            if (archivo.IdEstudiante.Length > 30 && !string.IsNullOrEmpty(archivo.IdEstudiante)) //Id Estudiante
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("IdEstudiante");
 
                 return false;
             }
-            if (archivo.NombreEstudiante.Length > 15) //Nombre Estudiante
+            if (archivo.NombreEstudiante.Length > 15 && !string.IsNullOrEmpty(archivo.NombreEstudiante)) //Nombre Estudiante
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("Nombre Estudiante");
 
                 return false;
             }
-            if (archivo.ApellidoEstudiante.Length > 15) //apellido estudiante
+            if (archivo.ApellidoEstudiante.Length > 15 && !string.IsNullOrEmpty(archivo.ApellidoEstudiante)) //apellido estudiante
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("apellido Estudiante");
 
                 return false;
             }
-            if (archivo.TipoIdEstudiante.Length > 30) //TipoIdEstudiante
+            if (archivo.TipoIdEstudiante.Length > 30 && !string.IsNullOrEmpty(archivo.TipoIdEstudiante)) //TipoIdEstudiante
             {
                 /*Imprimir donde fallo*/
                 System.Diagnostics.Debug.WriteLine("TipoIdEstudiante");
