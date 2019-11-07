c65cc1982f54dbc4c106c8b8dc67f30695386d1d
Berta Sánchez Jalet <berta.sanchez@ucr.ac.cr>
2019-11-07 12:30:32 -0600

    COD-67: Funciones actializadas. Se agregó la funcion recuperarPromedioCurso

--- AppIntegrador/Scripts/Visualizacion/graficosDashboard.js
+++ AppIntegrador/Scripts/Visualizacion/graficosDashboard.js
@@ -1,4 +1,8 @@
-﻿class GraficosDashboard {
+﻿//Berta Sánchez Jalet
+//COD-67: Desplegar la información del puntaje de un profesor y un curso específico.
+//Tarea técnica: Mostrar las nota resultantes.
+//Cumplimiento: 5/10
+class GraficosDashboard {
 
     constructor() { }
 
@@ -15,6 +19,24 @@
                 alert(resultados.promedio);
                 alert(resultados.cantidad);
             }
+
         });
     }
+
+    recuperarPromedioCursos() {
+        var correo = "@HttpContext.Current.User.Identity.Name";
+
+        $.ajax({
+            url: '/Dashboard/ObtenerPromedioCursos',
+            data: { correo: correo },
+            type: 'post',
+            dataType: 'json',
+            async: false,
+            success: function (resultados) {
+                alert(resultados.promedio);
+                alert(resultados.cantidad);
+            }
+        });
+    }
+
 }
