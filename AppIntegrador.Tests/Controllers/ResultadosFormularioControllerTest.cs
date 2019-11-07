1ccfe32fcd23e3fde881b0f0dbe6fce76eb607e7
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-11-07 08:45:48 -0600

    Re añadidos tests de unidad de Escamilla, Josué y míos

--- AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
+++ AppIntegrador.Tests/Controllers/ResultadosFormularioControllerTest.cs
@@ -15,7 +15,7 @@ namespace AppIntegrador.Tests.Controllers
         public void TestPromedioFormulario() //Comprueba que el promedio retornado no es nulo
         {
             ResultadosFormularioController controller = new ResultadosFormularioController();
-            String resultado = controller.getPromedio("00000001", "CI0128", 1, 2, 2019, "00000001") as String; //Metodo en el controlador y sus respectivos parametros
+            String resultado = controller.getPromedio("00000001", "CI0128", 1, 2, 2019, DateTime.Parse("2019-10-31"), DateTime.Parse("2019-11-02"), "00000001", "00000001") as String; //Metodo en el controlador y sus respectivos parametros
             Assert.IsNotNull(resultado); //Comprobacion de que no es null 
         }
 
@@ -26,9 +26,20 @@ namespace AppIntegrador.Tests.Controllers
             // Arrange
             ResultadosFormularioController controlador = new ResultadosFormularioController();
             // Act
-            String resultado = controlador.obtenerDesviacionEstandar("00000001", "CI0128", 1, 2, 2019, "00000001") as String; //Metodo en el controlador y sus respectivos parametros
+            String resultado = controlador.obtenerDesviacionEstandar("00000001", "CI0128", 1, 2, 2019, DateTime.Parse("2019-10-31"), DateTime.Parse("2019-11-02"), "00000001", "00000001") as String; //Metodo en el controlador y sus respectivos parametros
             // Assert
             Assert.IsNotNull(resultado); //Comprobacion de que no es null 
+        }
+
+        [TestMethod]
+        public void TestJustificacionNotNULL()
+        {
+            // Arrange
+            ResultadosFormularioController controller = new ResultadosFormularioController();
+            // Act
+            String result = controller.getJustificacionPregunta("00000001", "CI0128", 1, 2, 2019, DateTime.Parse("2019-10-31"), DateTime.Parse("2019-11-02"), "00000001", "00000001");
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
