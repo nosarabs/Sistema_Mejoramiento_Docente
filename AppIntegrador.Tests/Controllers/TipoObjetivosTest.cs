ab04390c30b5c948eef63019808810e3ab8ac033
Christian Asch <christian.asch@ucr.ac.cr>
2019-11-11 18:58:18 -0600

    Se hicieron más pruebas para el controlador TipoObjetivo y se alcanzó el 64.21% de code coverage en el mismo.

--- AppIntegrador.Tests/Controllers/TipoObjetivosTest.cs
+++ AppIntegrador.Tests/Controllers/TipoObjetivosTest.cs
@@ -32,6 +32,69 @@ namespace AppIntegrador.Tests.Controllers
             var toc = new TipoObjetivosController();
             var indexResult = toc.Index() as ViewResult;
             Assert.AreEqual("Index", indexResult.ViewName);
+            toc.Dispose();
+        }
+
+        [TestMethod]
+        public void DetailsNotNullTest()
+        {
+            String nombre = "Curso";
+            var controller = new TipoObjetivosController();
+            var result = controller.Details(nombre) as ViewResult;
+            Assert.IsNotNull(result);
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void DetailsNameTest()
+        {
+            String nombre = "Curso";
+            TipoObjetivosController controller = new TipoObjetivosController();
+            var result = controller.Details(nombre) as ViewResult;
+            Assert.AreEqual(result.ViewName, "Detalles");
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void DetailsNotFoundTest()
+        {
+            String nombre = "noexiste";
+            TipoObjetivosController controller = new TipoObjetivosController();
+            var result = controller.Details(nombre) as HttpNotFoundResult;
+            Assert.AreEqual(result.StatusCode, 404);
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void DetailsDataMockTest()
+        {
+            var mockdb = new Mock<DataIntegradorEntities>();
+            String nombre = "Curso";
+            TipoObjetivo to = new TipoObjetivo() { nombre = nombre };
+            mockdb.Setup(m => m.TipoObjetivo.Find(nombre)).Returns(to);
+
+            var controller = new TipoObjetivosController(mockdb.Object);
+            var result = controller.Details(nombre) as ViewResult;
+            Assert.AreEqual(result.Model, to);
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void CreateNotNullTest()
+        {
+            var controller = new TipoObjetivosController();
+            var result = controller.Create() as ViewResult;
+            Assert.IsNotNull(result);
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void CreateNameTest()
+        {
+            var controller = new TipoObjetivosController();
+            var result = controller.Create() as ViewResult;
+            Assert.AreEqual(result.ViewName, "Crear");
+            controller.Dispose();
         }
 
         [TestMethod]
@@ -61,7 +124,27 @@ namespace AppIntegrador.Tests.Controllers
         }
 
         [TestMethod]
-        public void TestDetailsDataMock()
+        public void EditNotNullTest()
+        {
+            var controller = new TipoObjetivosController();
+            String nombre = "Curso";
+            var result = controller.Edit(nombre) as ViewResult;
+            Assert.IsNotNull(result);
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void EditNameTest()
+        {
+            var controller = new TipoObjetivosController();
+            String nombre = "Curso";
+            var result = controller.Edit(nombre) as ViewResult;
+            Assert.AreEqual(result.ViewName, "Editar");
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void EditDataMockTest()
         {
             var mockdb = new Mock<DataIntegradorEntities>();
             String nombre = "Curso";
@@ -69,8 +152,43 @@ namespace AppIntegrador.Tests.Controllers
             mockdb.Setup(m => m.TipoObjetivo.Find(nombre)).Returns(to);
 
             var controller = new TipoObjetivosController(mockdb.Object);
-            var result = controller.Details(nombre) as ViewResult;
+            var result = controller.Edit(nombre) as ViewResult;
             Assert.AreEqual(result.Model, to);
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void DeleteNotNullTest()
+        {
+            var controller = new TipoObjetivosController();
+            String nombre = "Curso";
+            var result = controller.Delete(nombre) as ViewResult;
+            Assert.IsNotNull(result);
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void DeleteNameTest()
+        {
+            var controller = new TipoObjetivosController();
+            String nombre = "Curso";
+            var result = controller.Delete(nombre) as ViewResult;
+            Assert.AreEqual(result.ViewName, "Eliminar");
+            controller.Dispose();
+        }
+
+        [TestMethod]
+        public void DeleteDataMockTest()
+        {
+            var mockdb = new Mock<DataIntegradorEntities>();
+            String nombre = "Curso";
+            TipoObjetivo to = new TipoObjetivo() { nombre = nombre };
+            mockdb.Setup(m => m.TipoObjetivo.Find(nombre)).Returns(to);
+
+            var controller = new TipoObjetivosController(mockdb.Object);
+            var result = controller.Delete(nombre) as ViewResult;
+            Assert.AreEqual(result.Model, to);
+            controller.Dispose();
         }
 
         [TestMethod]
