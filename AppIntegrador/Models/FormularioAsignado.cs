dc123dcaa0609bae2261178ba91da8869f903f69
Steven Fernández Espinoza <victor.fernandezespinoza@ucr.ac.cr>
2019-11-23 01:13:26 -0600

    RIP-EDF1 Se integra el procedimiento con la nueva vista de formularios disponibles.
    Se crea una vista donde eventualmente los estudiantes van a ver los formularios que pueden llenar.
    Hace falta validar la parte de que los formularios no estén disponibles para llenar, pero que aparezcan.

---
+++ AppIntegrador/Models/FormularioAsignado.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppIntegrador.Models
+{
+    public class FormularioAsignado
+    {
+        public Periodo_activa_por Periodo { get; set; }
+        public String Nombre { get; set; }
+
+        public FormularioAsignado()
+        {
+            this.Periodo = null;
+            this.Nombre = "Formulario inválido";
+        }
+
+        public FormularioAsignado(Periodo_activa_por periodo)
+        {
+            // Guardar objeto de periodo
+            this.Periodo = periodo;
+
+            // Basado en el objeto, guardar nombre del formulario
+            using (DataIntegradorEntities db = new DataIntegradorEntities())
+            {
+                this.Nombre = db.Formulario.Find(this.Periodo.FCodigo).Nombre;
+                db.Dispose();
+            }
+        }
+    }
+}
