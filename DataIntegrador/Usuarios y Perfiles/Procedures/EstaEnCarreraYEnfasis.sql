7bb2a27f3f5ad29f192475c7ae583f0ea2c7f208
Sebastián Cruz <sebastian.cruzchavarria@ucr.ac.cr>
2019-11-25 00:10:25 -0600

    TAM 11.1: Se inició trabajo para mostrar en las vistas de usuarios solo lo que debería ver el usuario actual según su configuración de carrera y énfasis.

---
+++ DataIntegrador/Usuarios y Perfiles/Procedures/EstaEnCarreraYEnfasis.sql
@@ -0,0 +1,18 @@
+﻿/*TAM 11.1: Procedimiento que devuelve true si la persona dada está relacionada con la carrera y énfasis
+dados de alguna manera a través de uno o más perfiles, o false en caso contrario.*/
+
+CREATE PROCEDURE [dbo].[EstaEnCarreraYEnfasis]
+	@correoUsuario VARCHAR(50),
+	@codCarrera VARCHAR(10),
+	@codEnfasis VARCHAR(10),
+	@resultado BIT OUTPUT
+AS
+	IF EXISTS(
+		SELECT *
+		FROM UsuarioPerfil
+		WHERE Usuario = @correoUsuario AND CodCarrera = @codCarrera AND CodEnfasis = @codEnfasis
+	)
+SET @resultado = 1
+ELSE
+SET @resultado = 0
+
