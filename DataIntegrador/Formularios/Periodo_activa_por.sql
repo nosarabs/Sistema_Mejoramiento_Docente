0805917b7ac2f4b92dcc4b25c5fd437085915764
Bryan Ulate <bryan.ulate@ucr.ac.cr>
2019-11-26 09:48:52 -0600

    Agregado índice sobre la tabla Periodo_activa_por  para facilitar la búsqueda de formularios activos para los grupos en los que esté matriculado el estudiante. Cumple con 10 de lo planificado, pues se hacen las correcciones recomendadas por la profesora de bases.
    Co-Authored-By: Gabriel Galvez <gabriel.galvez@ucr.ac.cr>

--- DataIntegrador/Formularios/Periodo_activa_por.sql
+++ DataIntegrador/Formularios/Periodo_activa_por.sql
@@ -13,3 +13,6 @@
 	PRIMARY KEY(FCodigo, CSigla, GNumero, GAnno, GSemestre, FechaInicio, FechaFin),
 	CONSTRAINT fkPeriodoActivaPor FOREIGN KEY(FCodigo, CSigla, GNumero, GAnno, GSemestre) REFERENCES Activa_por(FCodigo, CSigla, GNumero, GAnno, GSemestre),
 )
+GO
+
+CREATE INDEX [IXPeriodoActivaPorGrupo] ON [dbo].[Periodo_activa_por] (CSigla, GNumero, GAnno, GSemestre) INCLUDE (FechaInicio, FechaFin, FCodigo)
