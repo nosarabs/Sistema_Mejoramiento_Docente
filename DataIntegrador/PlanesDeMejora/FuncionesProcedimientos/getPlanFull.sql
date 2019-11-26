827d01e21402969cd5dd95b1d07e010578acb3f1
Lucho Ramírez <luchoestebanra@gmail.com>
2019-11-26 10:53:46 -0600

    Indique la funcionalidad trabajada detallando las transacciones y los índices. -Creación de transacción de lectura de un plan de mejora completo con sus elementos relacionados.
    Indique el porcentaje de cumplimiento con respecto a lo planificado. Escala 0-10.
    -9/10

---
+++ DataIntegrador/PlanesDeMejora/FuncionesProcedimientos/getPlanFull.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[getPlanFull]
+	@codigoPlan int
+AS
+	BEGIN TRY
+		BEGIN TRANSACTION 
+			SELECT *
+			FROM PlanDeMejora p JOIN Objetivo o			ON o.codPlan = p.codigo
+								JOIN AccionDeMejora am	ON am.codPlan = o.codPlan AND
+														   am.nombreObj = o.nombre
+								JOIN Accionable a		ON a.codPlan = am.codPlan AND
+														   a.nombreObj = am.nombreObj AND
+														   a.descripAcMej = am.descripcion
+			WHERE @codigoPlan = p.codigo
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		PRINT 'No se pudo obtener el plan de mejora completo.';
+		THROW
+	END CATCH
+RETURN 0
