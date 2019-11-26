17648f77e0ed308b721074dcf4579751651940ba
Bryan Ulate <bryan.ulate@ucr.ac.cr>
2019-11-26 09:50:15 -0600

    Comentarios requeridos por la actividad supervisada.

--- DataIntegrador/Formularios/Periodo_activa_por.sql
+++ DataIntegrador/Formularios/Periodo_activa_por.sql
@@ -14,5 +14,6 @@
 	CONSTRAINT fkPeriodoActivaPor FOREIGN KEY(FCodigo, CSigla, GNumero, GAnno, GSemestre) REFERENCES Activa_por(FCodigo, CSigla, GNumero, GAnno, GSemestre),
 )
 GO
-
+-- Indexa la tabla para facilitar la búsqueda de formularios activos para los grupos en los que esté matriculado el estudiante. 
+-- Se incluye los campos de FechaInicio, FechaFin y FCodigo para hacer más rápida la recuperación de estos campos que también son requeridos.
 CREATE INDEX [IXPeriodoActivaPorGrupo] ON [dbo].[Periodo_activa_por] (CSigla, GNumero, GAnno, GSemestre) INCLUDE (FechaInicio, FechaFin, FCodigo)
