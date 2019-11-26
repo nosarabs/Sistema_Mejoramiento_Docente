fa0768d766b34cbd9e66fe2c353741307f6d5cbd
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-11-26 11:14:25 -0600

    Pair programming con Escamilla
    Procedimiento Almacenado:
    Insertar_Matriculado_En
    
    Nivel de aislamiento Serializable: Esto se debe a que no queremos que mientras
    matriculamos un nuevo estudiante, nos actualizen la información de él o que nos hagan una
    nueva inserción mientras leemos los datos.
    
    Se va a hacer un transacción por cada inserción de tupla, como la carga puede ser
    bastante amplia no deseamos atrasar tanto al sistema.

--- AppIntegrador/Controllers/Validador.cs
+++ AppIntegrador/Controllers/Validador.cs
@@ -1,4 +1,4 @@
-﻿using AppIntegrador.Models;
+﻿/*using AppIntegrador.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -75,4 +75,4 @@ namespace AppIntegrador.Controllers
         }
 
     }
-}
+}*/
