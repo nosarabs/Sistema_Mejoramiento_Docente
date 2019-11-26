d8bf1e0f3c3799018f6d909158e47b605de381fd
Marcial Carrillo <marcial.carrillo@ucr.ac.cr>
2019-11-26 10:29:11 -0600

    TAM-2.5: se implementa un transaction en ChangePassword con nivel de isolation repeatable read. Porcentaje de Cumplimiento: 5/10 Pair programming con Eric Rios

--- DataIntegrador/Usuarios y Perfiles/Procedures/ChangePassword.sql
+++ DataIntegrador/Usuarios y Perfiles/Procedures/ChangePassword.sql
@@ -4,6 +4,12 @@ CREATE PROCEDURE [dbo].[ChangePassword]
 	@username VARCHAR(50),
 	@newpassword VARCHAR(64)
 AS
+SET implicit_transactions OFF
+-- Aislamiento en repeatable read para asegurarse de que el correo no se cambie durante las consultas a la tabla, pero permitiendo lecturas concurrentes.
+SET TRANSACTION isolation level repeatable READ
+begin try
+begin TRANSACTION
+begin
 	DECLARE @salt VARCHAR(64)
 
 	SELECT @salt=salt
@@ -14,4 +20,16 @@ AS
 	SET Password = HASHBYTES('SHA2_256', @newpassword+CAST(@salt AS VARCHAR(36)))
 	Where username=@username
 
+END
+COMMIT TRANSACTION
+SET implicit_transactions on
+SET TRANSACTION isolation level READ COMMITTED
 RETURN 0
+END TRY
+BEGIN CATCH
+    PRINT ERROR_MESSAGE()
+	ROLLBACK TRANSACTION
+	SET implicit_transactions on
+	SET TRANSACTION isolation level READ COMMITTED
+	RETURN 1
+END CATCH
