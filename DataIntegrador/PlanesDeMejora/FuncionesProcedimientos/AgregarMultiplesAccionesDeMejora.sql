49b7bd14f1a36dfba56933ee77841efeac6f083e
Johan.Cordoba <johan.cordoba@ucr.ac.cr>
2019-11-26 11:44:12 -0600

    Se hizo el procedimiento para AgregarMultiplesAccionesDeMejora que utiliza una transacción para insertar múltiples acciones a la vez. (10/10) Esto es parte de un refactoring de la actividad supervisada del 26 de noviembre.

---
+++ DataIntegrador/PlanesDeMejora/FuncionesProcedimientos/AgregarMultiplesAccionesDeMejora.sql
@@ -0,0 +1,27 @@
+﻿CREATE TYPE AccionDeMejoraParametro 
+AS TABLE(
+	codPlan int not null,
+	nombreObj varchar(50) not null,
+	descripcion varchar(250) not null,
+	fechaInicio date,
+	fechaFin date,
+	codPlantilla int,
+	borrado bit
+	)
+GO
+
+CREATE PROCEDURE [dbo].[AgregarMultiplesAccionesDeMejora]
+	@Acciones AccionDeMejoraParametro READONLY
+AS
+	DECLARE @STATE INT = 0
+	BEGIN TRY
+			BEGIN TRANSACTION
+				INSERT INTO AccionDeMejora
+				SELECT * FROM @Acciones;
+			COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		SELECT @STATE = ERROR_NUMBER()
+		ROLLBACK TRANSACTION
+	END CATCH
+RETURN @STATE
