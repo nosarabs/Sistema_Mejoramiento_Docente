9c678f239adc188be10ce4739acd8fc856b552a7
Christian Asch <christian.asch@ucr.ac.cr>
2019-11-26 11:39:45 -0600

    Se terminó el procedimiento de AgregarMultiplesObjetivos que utiliza una transacción. (10/10) Esto es parte de un refactoring.

--- DataIntegrador/PlanesDeMejora/FuncionesProcedimientos/AgregarMultiplesObjetivos.sql
+++ DataIntegrador/PlanesDeMejora/FuncionesProcedimientos/AgregarMultiplesObjetivos.sql
@@ -14,6 +14,15 @@ GO
 CREATE PROCEDURE [dbo].AgregarMultiplesObjetivos
 	@Objetivos ObjetivoParametro READONLY
 	AS
-	SELECT *
-	from @Objetivos
-RETURN 0
+	declare @status int = 0;
+	begin try
+		begin transaction
+			insert into Objetivo
+			select * from @Objetivos;
+		commit transaction
+	end try
+	begin catch
+		select @status = ERROR_NUMBER()
+		rollback transaction
+	end catch;
+RETURN @status;
