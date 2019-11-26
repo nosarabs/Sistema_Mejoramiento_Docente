e5d12fcba34a3d33adb5b1e47b14c05c6d4255eb
Crexzyz <victor.fernandezespinoza@ucr.ac.cr>
2019-11-26 11:12:05 -0600

    Actividad supervisada Rama: Diego
    Pair programming con Diego Orozco
    
    Historia: RIP-EDF1
    Se genera una transacción sobre el procedimiento que guarda respuestas a preguntas de respuesta libre
    
    Se valida con los profesores la creacion y el porqué de la transacción y se aprueba.

--- DataIntegrador/Formularios/Procedimientos/GuardarRespuestaAPreguntaLibre.sql
+++ DataIntegrador/Formularios/Procedimientos/GuardarRespuestaAPreguntaLibre.sql
@@ -11,17 +11,26 @@
 	@texto nvarchar(250) = NULL
 AS
 BEGIN
-	MERGE INTO Responde_respuesta_libre AS Target
-	USING (VALUES
-		(@codFormulario, @correo, @siglaCurso, @numGrupo, @anno, @semestre, @fecha, @codPregunta, @codseccion, @texto)
-	)
-	AS SOURCE ([FCodigo],[Correo],[CSigla],[GNumero],[GAnno],[GSemestre],[Fecha],[PCodigo], [SCodigo], [Observacion])
-	ON Target.FCodigo = Source.FCodigo and Target.Correo = Source.Correo and 
-	   Target.CSigla  = Source.CSigla  and Target.GNumero = Source.GNumero and 
-	   Target.GAnno = Source.GAnno and Target.GSemestre = Source.GSemestre and 
-	   Target.Fecha = Source.Fecha and Target.PCodigo = Source.PCodigo and
-	   Target.SCodigo = Source.SCodigo
-	WHEN NOT MATCHED BY TARGET THEN
-	INSERT ([FCodigo],[Correo],[CSigla],[GNumero],[GAnno],[GSemestre],[Fecha],[PCodigo], [SCodigo], [Observacion])
-	VALUES (@codFormulario, @correo, @siglaCurso, @numGrupo, @anno, @semestre, @fecha, @codPregunta, @codseccion, @texto);
+	BEGIN TRY
+		BEGIN TRANSACTION
+		MERGE INTO Responde_respuesta_libre AS Target
+		USING (VALUES
+			(@codFormulario, @correo, @siglaCurso, @numGrupo, @anno, @semestre, @fecha, @codPregunta, @codseccion, @texto)
+		)
+		AS SOURCE ([FCodigo],[Correo],[CSigla],[GNumero],[GAnno],[GSemestre],[Fecha],[PCodigo], [SCodigo], [Observacion])
+		ON Target.FCodigo = Source.FCodigo and Target.Correo = Source.Correo and 
+		   Target.CSigla  = Source.CSigla  and Target.GNumero = Source.GNumero and 
+		   Target.GAnno = Source.GAnno and Target.GSemestre = Source.GSemestre and 
+		   Target.Fecha = Source.Fecha and Target.PCodigo = Source.PCodigo and
+		   Target.SCodigo = Source.SCodigo
+		WHEN NOT MATCHED BY TARGET THEN
+		INSERT ([FCodigo],[Correo],[CSigla],[GNumero],[GAnno],[GSemestre],[Fecha],[PCodigo], [SCodigo], [Observacion])
+		VALUES (@codFormulario, @correo, @siglaCurso, @numGrupo, @anno, @semestre, @fecha, @codPregunta, @codseccion, @texto);
+
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		SELECT ERROR_NUMBER()
+		ROLLBACK TRANSACTION
+	END CATCH
 END;
