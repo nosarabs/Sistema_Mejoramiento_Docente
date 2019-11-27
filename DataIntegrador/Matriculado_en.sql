81408b318bc14d664d91f6e53d44866105dec6af
Steven Fernández Espinoza <victor.fernandezespinoza@ucr.ac.cr>
2019-11-27 01:30:13 -0600

    Arreglo en el post-deployment script

--- DataIntegrador/Matriculado_en.sql
+++ DataIntegrador/Matriculado_en.sql
@@ -6,6 +6,6 @@
 	[Semestre] TINYINT NOT NULL,
 	[Anno] INT NOT NULL,
 	PRIMARY KEY (CorreoEstudiante, SiglaCurso, NumGrupo, Semestre, Anno),
-	FOREIGN KEY (CorreoEstudiante) REFERENCES Estudiante (Correo) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (SiglaCurso, NumGrupo, Semestre, Anno) REFERENCES Grupo (SiglaCurso, NumGrupo, Semestre, Anno) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT FK_Matricula_correo FOREIGN KEY (CorreoEstudiante) REFERENCES Estudiante (Correo) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT FK_Matricula_grupo FOREIGN KEY (SiglaCurso, NumGrupo, Semestre, Anno) REFERENCES Grupo (SiglaCurso, NumGrupo, Semestre, Anno) ON UPDATE CASCADE ON DELETE CASCADE
 )
