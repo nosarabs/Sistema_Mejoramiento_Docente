c35fe840f3f63a1755b0e1b60ab7a8ecc3d90b18
Berta Sánchez Jalet <berta.sanchez@ucr.ac.cr>
2019-11-27 20:48:51 -0600

    COD-78 y COD-79: avance con despliegue de gráficos. Nueva función generarGraficos en graficosDashboard.js Problemas a la hora de "pintarlo".

--- AppIntegrador/Scripts/Visualizacion/graficosDashboard.js
+++ AppIntegrador/Scripts/Visualizacion/graficosDashboard.js
@@ -51,4 +51,100 @@ class GraficosDashboard {
         return [promedio, cantidad]
     }
 
+    generarGrafico(canvas) {
+        return 
+        var colors = chroma.scale(["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"]).colors(dataLength);
+        //chroma.js: https://gka.github.io/chroma.js/
+
+        new Chart(canvas, {
+
+            type: "bar",
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [
+                    {
+                        label: "Cantidad de estudiantes",
+                        backgroundColor: colors,
+                        hoverBackgroundColor: colors,
+                        borderColor: "black",
+                        borderWidth: 0,
+                        data: [12, 19, 3, 5, 2, 3]
+                    }
+                ]
+            },
+            options: {
+                layout: {
+                    padding: {
+                        left: 0,
+                        right: 0,
+                        top: 30,
+                        bottom: 0
+                    }
+                },
+                legend: {
+                    display: false,
+                    labels: {
+                        fontColor: "black",
+                        fontSize: 16,
+                    }
+                },
+                title: {
+                    display: false,
+                },
+                tooltips: {
+                    enabled: false
+                },
+                responsive: true,
+                maintainAspectRatio: true,
+                devicePixelRatio: 2,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1,
+                            fontColor: "#747474",
+                            fontSize: 16,
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            fontColor: "#747474",
+                            fontSize: 16,
+                        }
+                    }]
+                },
+                plugins: {
+                    datalabels: {
+                        display: function (context) {
+                            return context.dataset.data[context.dataIndex] !== 0;
+                        },
+                        color: "#747474",
+                        textStrokeColor: "black",
+                        textStrokeWidth: 0,
+                        anchor: "end",
+                        align: "start",
+                        clamp: true,
+                        offset: -30,
+                        textAlign: "center",
+                        font: {
+                            size: "16",
+                            weight: "normal"
+                        },
+                        formatter: function (value, ctx) {
+                            var sum = 0;
+                            var data = ctx.chart.data.datasets[0].data;
+                            for (var i = 0; i < data.length; ++i) {
+                                sum += data[i];
+                            }
+                            var percentage = ((value * 100 / sum).toFixed(2) + "%").replace(".", ",");
+                            return percentage;
+                        }
+                    }
+                }
+            }
+
+        });
+
+    }
+
 }
