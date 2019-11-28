c7750b6d67d0b9ab5e08abaf32a1b858ade35911
Diego-Orozco <diego.orozco@ucr.ac.cr>
2019-11-28 16:40:44 -0600

    RIP-AF3: Se mejora la vista de asignacion de formularios. Queda pendiente hacer el procedimiento de asociar a un curso, pero se espera porque se debe de realizar un cambio a la base de datos.

--- AppIntegrador/Controllers/AsignacionFormulariosController.cs
+++ AppIntegrador/Controllers/AsignacionFormulariosController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AppIntegrador.Models;
+
+
 
 namespace AppIntegrador.Controllers
 {
     public class AsignacionFormulariosController : Controller
     {
+        private DataIntegradorEntities db;
+
         private DashboardController dashboard =  new DashboardController();
+
+        public AsignacionFormulariosController()
+        {
+            db = new DataIntegradorEntities();
+        }
         // GET: Asignacion
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(string id)
         {
-            var x = 1;
+            if (HttpContext == null)
+            {
+                return Redirect("~/");
+            }
+            Formulario formularioDB = db.Formulario.Find(id);
+            if (formularioDB == null)
+            {
+                return View();
+            }
+
+            ViewBag.Nombre = formularioDB.Nombre;
             return View();
         }
 
@@ -25,7 +45,7 @@ namespace AppIntegrador.Controllers
             // para que acá sean implementados
             
             // llamado al procedimiento almacenado 
-            return View();
+            return View("Index");
         }
     }
 }
