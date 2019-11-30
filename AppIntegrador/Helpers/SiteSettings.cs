60886f577216bc93e3ac0b6502e3591c6ed853f5
Tatiana Briones <tatiana.briones@ucr.ac.cr>
2019-11-30 13:38:35 -0600

    TAM 16.1 Primera version de la implementacion de los servicios de captcha.
    Falta refactorizar HomeController.

---
+++ AppIntegrador/Helpers/SiteSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppIntegrador.Helpers
+{
+    public static class SiteSettings
+    {
+        public const string GoogleRecaptchaSecretKey = "6LftXMUUAAAAANjYycZgn7InPwI4n9vunLZ41lCd";
+        public const string GoogleRecaptchaSiteKey = "6LftXMUUAAAAAEcP9jIDTrfMhCYkovsa5VKHig9T";
+    }
+}
