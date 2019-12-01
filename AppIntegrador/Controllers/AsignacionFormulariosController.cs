bb6a0a3732d3d2096ded20b18679ea83786a59ac
jackster31415 <jostin.alvarez@ucr.ac.cr>
2019-11-30 18:24:26 -0600

    Se agrega el procedimiento almacenado para encontrar los estudiantes que están asociados a un formulario. También se agrega el código inicial para enviarle un correo a esos estudiantes

--- AppIntegrador/Controllers/AsignacionFormulariosController.cs
+++ AppIntegrador/Controllers/AsignacionFormulariosController.cs
@@ -13,7 +13,7 @@ namespace AppIntegrador.Controllers
     {
         private DataIntegradorEntities db;
 
-        private DashboardController dashboard =  new DashboardController();
+        private DashboardController dashboard = new DashboardController();
 
         public AsignacionFormulariosController()
         {
@@ -43,9 +43,36 @@ namespace AppIntegrador.Controllers
             // RIP-AF3
             // Como es la asignacion por curso, se asume que viene null los demas 
             // para que acá sean implementados
-            
+
             // llamado al procedimiento almacenado 
             return View("Index");
+        }
+
+        // Modificado por: Jostin Álvarez
+        // Historia a la que pertenece: RIP-AFC "Yo como administrativo quiero enviar un correo a los estudiantes para que llenen formularios cuando se los asigno"
+        // Envía un correo cada estudiando que está asignado a un cuestionario pidiéndole que lo llene.
+        private void EnviarCorreoSobreAsignaciónCuestionario(Formulario formulario)
+        {
+            List<string> involucrados = new List<string>();
+
+            // Se obtienen todos los estudiantes a los que les tiene que llegar el correo.
+            List<Estudiante> estudiantes = db.ObtenerEstudiantesAsociadosAFormulario();
+            foreach (Estudiante estudiante in estudiantes)
+            {
+                involucrados.Add(estudiante.Correo);
+            }
+            Utilities.EmailNotification emailNotification = new Utilities.EmailNotification();
+
+            string asunto = "Evaluación del formulario " + formulario.Nombre;
+
+            string texto = "Estimado/a estudiante\n";
+            texto += "La Escuela de " + formulario.fechaInicio.ToString();
+            texto += "<br>Favor no responder directamente a este correo";
+
+            string textoAlt = "<body><p>" + texto + "</p></body>";
+
+            // Se envía el correo formado a todos los estudiantes involucrados
+            _ = emailNotification.SendNotification(involucrados, asunto, texto, textoAlt);
         }
     }
 }
