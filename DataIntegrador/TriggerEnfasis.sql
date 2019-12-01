b7f7f4f3a00f5ef52c9c9cc3d6991a99bb448e0d
Eric Rios <eric.rios@ucr.ac.cr>
2019-11-30 22:35:46 -0600

    Al agregar un énfasis se configuran todos los permisos para Superusuario y admin@mail.com. TAM-12.1, TAM-12.2, TAM-12.3, TAM-12.4.
    El trigger de inserción de énfasis asigna todos los permisos al perfil Superusuario.
    Así mismo, se le asigna el perfil de Superusuario a admin@mail.com en ese énfasis.
    En consecuencia, se modifica el post-deployment script para quitar inserciones innecesarias y mover la inserción de Personas, Usuarios, Perfiles y Permisos al inicio del script.

--- DataIntegrador/TriggerEnfasis.sql
+++ DataIntegrador/TriggerEnfasis.sql
@@ -2,19 +2,56 @@
 	ON [dbo].[Enfasis]
 	INSTEAD OF INSERT
 	AS
-	DECLARE @codigo varchar(10), @codigoCarrera varchar(10)
+	DECLARE @codigo varchar(10), @codigoCarrera varchar(10), @nombre varchar(50), @permisoId int
 	DECLARE cursor_enfasis CURSOR
-	FOR SELECT Codigo, CodCarrera
+	FOR SELECT Codigo, CodCarrera, Nombre
 	FROM inserted;
+	-- Para asignar todos los permisos en este énfasis a Superusuario
+	DECLARE cursor_permiso_enfasis CURSOR FOR
+	SELECT Id
+	FROM Permiso;
+
 	OPEN cursor_enfasis
-	FETCH NEXT FROM cursor_enfasis INTO @codigo, @codigoCarrera
+	FETCH NEXT FROM cursor_enfasis INTO @codigo, @codigoCarrera, @nombre
 	WHILE @@FETCH_STATUS = 0
 		BEGIN
 			IF(NOT EXISTS (SELECT * FROM Enfasis WHERE Codigo = @codigo and CodCarrera = @codigoCarrera) and @codigo not like '' and @codigoCarrera not like '')
 			BEGIN
-				INSERT INTO Enfasis SELECT * FROM inserted
+				INSERT INTO Enfasis(CodCarrera, Codigo, Nombre)
+				VALUES (@codigoCarrera, @codigo, @nombre)
+
+				-- Colocar cursor de permisos al inicio
+				OPEN cursor_permiso_enfasis
+				FETCH NEXT FROM cursor_permiso_enfasis INTO @permisoId
+				-- Dar todos los permisos a Superusuario
+				WHILE @@FETCH_STATUS = 0
+					BEGIN
+						-- No insertar duplicados
+						IF NOT EXISTS (
+							SELECT *
+							FROM PerfilPermiso
+							WHERE PermisoId = @permisoId and Perfil = 'Superusuario' and CodCarrera = @codigoCarrera and CodEnfasis = @codigo
+						)
+							BEGIN
+								INSERT INTO PerfilPermiso (PermisoId, Perfil, CodCarrera, CodEnfasis)
+								VALUES (@permisoId, 'Superusuario', @codigoCarrera, @codigo)
+							END
+					FETCH NEXT FROM cursor_permiso_enfasis INTO @permisoId
+					END
+				CLOSE cursor_permiso_enfasis
+				-- Asignar a admin@mail.com el perfil de Superusuario en este énfasis
+				IF NOT EXISTS (
+					SELECT *
+					FROM UsuarioPerfil
+					WHERE Usuario = 'admin@mail.com' and Perfil = 'Superusuario' and CodCarrera = @codigoCarrera and CodEnfasis = @codigo
+				)
+					BEGIN
+						INSERT INTO UsuarioPerfil (Usuario, Perfil, CodCarrera, CodEnfasis)
+						VALUES ('admin@mail.com', 'Superusuario', @codigoCarrera, @codigo)
+					END
 			END
-			FETCH NEXT FROM cursor_enfasis INTO @codigo, @codigoCarrera
+			FETCH NEXT FROM cursor_enfasis INTO @codigo, @codigoCarrera, @nombre
 		END
 	CLOSE cursor_enfasis
 	DEALLOCATE cursor_enfasis
+	DEALLOCATE cursor_permiso_enfasis
