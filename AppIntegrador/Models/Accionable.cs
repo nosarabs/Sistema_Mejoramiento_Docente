04a9a1a8960591af612a29e33bd2e7dfc42313a0
Lucho Ramírez <luchoestebanra@gmail.com>
2019-12-02 12:01:27 -0600

    Actualización del modelo.edmx

--- AppIntegrador/Models/Accionable.cs
+++ AppIntegrador/Models/Accionable.cs
@@ -26,9 +26,10 @@ namespace AppIntegrador.Models
         public string descripcion { get; set; }
         public Nullable<System.DateTime> fechaInicio { get; set; }
         public Nullable<System.DateTime> fechaFin { get; set; }
-        public Nullable<int> tipo { get; set; }
+        public string tipo { get; set; }
         public Nullable<int> peso { get; set; }
         public Nullable<int> pesoPorcentaje { get; set; }
+    
         public virtual AccionDeMejora AccionDeMejora { get; set; }
         public virtual AccionableEscala AccionableEscala { get; set; }
         public virtual AccionablePorcentaje AccionablePorcentaje { get; set; }
