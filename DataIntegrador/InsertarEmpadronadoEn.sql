2c6be7836fcd245af65237b8bb46cf69dae8924d
Denisse Alfaro <denisse.alfaro@ucr.ac.cr>
2019-12-02 13:23:34 -0600

    COD-70: Modificaciones procedimientos almacenados. Se elimina todo lo que tenga que ver con la tabla imparte

--- DataIntegrador/InsertarEmpadronadoEn.sql
+++ DataIntegrador/InsertarEmpadronadoEn.sql
@@ -4,6 +4,10 @@
 	@CodEnfasis VARCHAR(10)
 AS
 begin
-	insert into Empadronado_en (CorreoEstudiante, CodEnfasis, CodCarrera)
-	values (@CorreoEstudiante,@CodEnfasis,@CodCarrera)
+	--Esta condicion revisa que no se intente empadronar estudiantes no existentes/enfasis no existentes
+	IF(@CorreoEstudiante IN (SELECT Correo FROM Estudiante) AND EXISTS (SELECT * FROM Enfasis WHERE CodCarrera = @CodCarrera and Codigo = @CodEnfasis))
+	BEGIN
+		insert into Empadronado_en (CorreoEstudiante, CodEnfasis, CodCarrera)
+		values (@CorreoEstudiante,@CodEnfasis,@CodCarrera)
+	END
 end
