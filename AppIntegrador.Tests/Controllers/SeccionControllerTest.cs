ec096083d1914a6b8d8991a896ff53b3729ef433
Bryan Ulate <bryan.ulate@ucr.ac.cr>
2019-12-03 23:05:22 -0600

    Agregadas pruebas de unidad al controlador de secciones. Se alcanza 80% de cobertura.

--- AppIntegrador.Tests/Controllers/SeccionControllerTest.cs
+++ AppIntegrador.Tests/Controllers/SeccionControllerTest.cs
@@ -22,109 +22,128 @@ namespace AppIntegrador.Tests.Controllers
     [TestClass]
     public class SeccionControllerTest
     {
-        //[TestMethod]
-        //// Prueba de que la vista no sea nula
-        //public void TestIndexNotNull()
-        //{
-        //    // Arrange
-        //    SeccionController seccionController = new SeccionController();
+        [TestMethod]
+        // Prueba de que la vista no sea nula
+        public void TestIndexNotNull()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
 
-        //    // Act
-        //    ViewResult result = seccionController.Index("","","") as ViewResult;
+            // Act
+            ViewResult result = seccionController.Index("", "", "") as ViewResult;
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
-        //[TestMethod]
-        //// Prueba de que la vista no sea nula
-        //public void TestCreateNotNull()
-        //{
-        //    // Arrange
-        //    SeccionController seccionController = new SeccionController();
+        [TestMethod]
+        // Prueba de que la vista no sea nula
+        public void TestCreateNotNull()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
 
-        //    // Act
-        //    ViewResult result = seccionController.Create() as ViewResult;
+            // Act
+            ViewResult result = seccionController.Create() as ViewResult;
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
-        //[TestMethod]
-        ////RIP CF5
-        //// Prueba de que la vista no sea nula
-        //public void TestCreateIndexFiltroCodigoNotNull()
-        //{
-        //    // Arrange
-        //    SeccionController seccionController = new SeccionController();
-
-        //    // Act
-        //    ViewResult result = seccionController.Index("0000001","","") as ViewResult;
-
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
-
-        //[TestMethod]
-        ////RIP CF5
-        //// Prueba de que la vista no sea nula
-        //public void TestCreateIndexFiltroNotNull()
-        //{
-        //    // Arrange
-        //    SeccionController seccionController = new SeccionController();
-
-        //    // Act
-        //    ViewResult result = seccionController.Index(null,null,null) as ViewResult;
-
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
-
-        //[TestMethod]
-        ////RIP CF5
-        //// Prueba de que la vista no sea nula
-        //public void TestIndexTipos()
-        //{
-        //    // Arrange
-        //    SeccionController seccionController = new SeccionController();
-
-        //    // Se prueban las de si/no/nr
-        //    ViewResult result = seccionController.Index("", "", "S") as ViewResult;
-        //    Assert.IsNotNull(result);
-
-        //    // Se prueban las escalares
-        //    result = seccionController.Index("", "", "E") as ViewResult;
-        //    Assert.IsNotNull(result);
-
-        //    // Se prueban las de respuesta libre
-        //    result = seccionController.Index("", "", "L") as ViewResult;
-        //    Assert.IsNotNull(result);
-
-        //    // Se prueban las de seleccion multiple
-        //    result = seccionController.Index("", "", "M") as ViewResult;
-        //    Assert.IsNotNull(result);
-
-        //    // Se prueba las de seleccion unica
-        //    result = seccionController.Index("", "", "U") as ViewResult;
-        //    Assert.IsNotNull(result);
-
-        //}
-
-        //[TestMethod]
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
 
-        ////RIP CF5
-        //// Prueba de que la vista no sea nula
-        //public void TestCreateIndexFiltroEnunciadoNotNull()
-        //{
-        //    // Arrange
-        //    SeccionController seccionController = new SeccionController();
+        [TestMethod]
+        //RIP CF5
+        // Prueba de que la vista no sea nula
+        public void TestCreateIndexFiltroCodigoNotNull()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
 
-        //    // Act
-        //    ViewResult result = seccionController.Index("", "Info", "") as ViewResult;
+            // Act
+            ViewResult result = seccionController.Index("0000001", "", "") as ViewResult;
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        //RIP CF5
+        // Prueba de que la vista no sea nula
+        public void TestCreateIndexFiltroNotNull()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
+
+            // Act
+            ViewResult result = seccionController.Index(null, null, null) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        //RIP CF5
+        // Prueba de que la vista no sea nula
+        [TestMethod]
+        public void TestIndexSiNo()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
+
+            // Se prueban las de si/no/nr
+            ViewResult result = seccionController.Index("", "", "S") as ViewResult;
+            Assert.IsNotNull(result);
+
+        }
+
+        public void TestIndexEscalar()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
+            // Se prueban las escalares
+            var result = seccionController.Index("", "", "E") as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        public void TestIndexRespuestaLibre()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
+            // Se prueban las de respuesta libre
+            var result = seccionController.Index("", "", "L") as ViewResult;
+            Assert.IsNotNull(result);
+        }
+        public void TestIndexSeleccionMultiple()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
+            // Se prueban las de seleccion multiple
+            var result = seccionController.Index("", "", "M") as ViewResult;
+            Assert.IsNotNull(result);
+        }
+        public void TestIndexSeleccionUnica()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
+
+            // Se prueba las de seleccion unica
+            var result = seccionController.Index("", "", "U") as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        //RIP CF5
+        // Prueba de que la vista no sea nula
+        public void TestCreateIndexFiltroEnunciadoNotNull()
+        {
+            // Arrange
+            SeccionController seccionController = new SeccionController();
+
+            // Act
+            ViewResult result = seccionController.Index("", "Info", "") as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
 
         [TestMethod]
         public void TestCreateFilterNullNotNull()
@@ -218,6 +237,82 @@ namespace AppIntegrador.Tests.Controllers
 
             Assert.IsNull(result);
         }
+        [TestMethod]
+        public void ProbarActualizarBancoSeccionesNullParams()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.ActualizarBancoSecciones(null, null, null) as PartialViewResult;
+
+            Assert.AreEqual("Ninguno", result.ViewBag.filtro);
+        }
+
+        [TestMethod]
+        public void ProbarActualizarBancoSeccionesFiltroCodigo()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.ActualizarBancoSecciones("1", "00000001", "") as PartialViewResult;
+
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void ProbarActualizarBancoSeccionesFiltroNombre()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.ActualizarBancoSecciones("2", "", "Cuál") as PartialViewResult;
+
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void ProbarActualizarBancoSeccionesStringVacios()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.ActualizarBancoSecciones("", "", "") as PartialViewResult;
+
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void ProbarActualizarCrearSeccionNoNulo()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.ActualizarCrearSeccion() as PartialViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ProbarObtenerSeccionesConPreguntasEditable()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.ObtenerSeccionesConPreguntasEditable("00000001") ;
+
+            Assert.IsNotNull(result.Count);
+        }
+
+        [TestMethod]
+        public void ProbarVistaPreviaParametroNulo()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.VistaPrevia(null) as HttpStatusCodeResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ProbarVistaPreviaSeccionValida()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.VistaPrevia("00000001") as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ProbarVistaPreviaSeccionInvalida()
+        {
+            SeccionController controller = new SeccionController();
+            var result = controller.VistaPrevia("NOEXISTE") as HttpNotFoundResult;
+            Assert.IsNotNull(result);
+        }
+
+
 
         [TestInitialize]
         public void Init()
