fefd768c74775cea206d1f501dbbb42dcaf83f6c
jackster31415 <jostin.alvarez@ucr.ac.cr>
2019-12-04 12:27:53 -0600

    Cambios en el procedimiento almacenado para obtener estudiantes

--- DataIntegrador/Formularios/Procedimientos/ObtenerEstudiantesAsociadosAFormulario.sql
+++ DataIntegrador/Formularios/Procedimientos/ObtenerEstudiantesAsociadosAFormulario.sql
@@ -16,83 +16,93 @@ CREATE PROCEDURE [dbo].[ObtenerEstudiantesAsociados]
 	@CorreoProfesor	varchar(50)
 )
 AS
-SELECT * FROM Estudiante;
-/*
 	--Unidad academica
 	IF (@CodigoUnidadAcademica IS NOT NULL AND @CodigoCarrera IS NULL AND @CodigoEnfasis IS NULL AND @SiglaCurso IS NULL AND @NumGrupo IS NULL AND	@Semestre IS NULL AND @Anno IS NULL AND @CorreoProfesor IS NULL)
-			SELECT Grupo.SiglaCurso, Grupo.NumGrupo, Grupo.Semestre, Grupo.Anno
-			FROM UnidadAcademica JOIN Inscrita_en ON UnidadAcademica.Codigo = Inscrita_en.CodUnidadAc 
-									JOIN Carrera ON Carrera.Codigo = Inscrita_en.CodCarrera 
-									JOIN Enfasis ON Enfasis.CodCarrera = Carrera.Codigo
-									JOIN Pertenece_a ON Pertenece_a.CodCarrera = Carrera.Codigo AND Pertenece_a.CodEnfasis = Enfasis.Codigo
-									JOIN Curso ON Curso.Sigla = Pertenece_a.SiglaCurso
-									JOIN Grupo ON Grupo.SiglaCurso = Curso.Sigla
-			WHERE UnidadAcademica.Codigo = @CodigoUnidadAcademica
+		SELECT * FROM Estudiante e
+									JOIN UnidadAcademica ua on ua.Codigo = @CodigoCarrera
+									JOIN Matriculado_en me ON me.CorreoEstudiante = e.Correo
+									JOIN Inscrita_en ie ON ua.Codigo = ie.CodUnidadAc 
+									JOIN Carrera ca ON ca.Codigo = ie.CodCarrera 
+									JOIN Enfasis en ON en.CodCarrera = ca.Codigo
+									JOIN Pertenece_a pa ON pa.CodCarrera = ca.Codigo AND pa.CodEnfasis = en.Codigo
+									JOIN Curso c ON c.Sigla = pa.SiglaCurso AND me.SiglaCurso = c.Sigla
+									JOIN Grupo g ON g.SiglaCurso = c.Sigla AND g.NumGrupo = me.NumGrupo
+									JOIN Enfasis ON en.CodCarrera = ca.Codigo
+									
+		WHERE ua.Codigo = @CodigoUnidadAcademica
+
 
 	-- Carrera-Enfasis
 	IF (@CodigoUnidadAcademica IS NULL AND @CodigoCarrera IS NOT NULL AND @CodigoEnfasis IS NOT NULL AND @SiglaCurso IS NULL AND @NumGrupo IS NULL AND	@Semestre IS NULL AND @Anno IS NULL AND @CorreoProfesor IS NULL )
-			SELECT Grupo.SiglaCurso, Grupo.NumGrupo, Grupo.Semestre, Grupo.Anno
-			FROM UnidadAcademica JOIN Inscrita_en ON  UnidadAcademica.Codigo = Inscrita_en.CodUnidadAc 
-								 JOIN Carrera ON Carrera.Codigo = Inscrita_en.CodCarrera 
-								 JOIN Enfasis ON Enfasis.CodCarrera = Carrera.Codigo
-								 JOIN Pertenece_a ON Pertenece_a.CodCarrera = Carrera.Codigo AND Pertenece_a.CodEnfasis = Enfasis.Codigo
-								 JOIN Curso ON Curso.Sigla = Pertenece_a.SiglaCurso
-								 JOIN Grupo ON Grupo.SiglaCurso = Curso.Sigla
-			WHERE Carrera.Codigo = @CodigoCarrera AND Enfasis.Codigo = @CodigoEnfasis
+		SELECT e.Correo, e.Carne FROM UnidadAcademica ua 
+									JOIN Inscrita_en ie ON ua.Codigo = ie.CodUnidadAc 
+									JOIN Carrera ca ON ca.Codigo = ie.CodCarrera 
+									JOIN Enfasis en ON en.CodCarrera = ca.Codigo
+									JOIN Pertenece_a pa ON pa.CodCarrera = ca.Codigo AND pa.CodEnfasis = en.Codigo
+									JOIN Curso c ON c.Sigla = pa.SiglaCurso
+									JOIN Grupo g ON g.SiglaCurso = c.Sigla
+									JOIN Matriculado_en me ON me.SiglaCurso = c.Sigla
+									JOIN Estudiante e ON me.CorreoEstudiante = e.Correo
+
+		WHERE ca.Codigo = @CodigoCarrera AND en.Codigo = @CodigoEnfasis
 
 	--Profesor 
 	IF (@CodigoUnidadAcademica IS NULL AND @CodigoCarrera IS  NULL AND @CodigoEnfasis IS  NULL AND @SiglaCurso IS NULL AND @NumGrupo IS NULL AND @Semestre IS NULL AND @Anno IS NULL AND @CorreoProfesor IS NOT NULL)
-			SELECT Grupo.SiglaCurso, Grupo.NumGrupo, Grupo.Semestre, Grupo.Anno
-			FROM Profesor JOIN Imparte ON Profesor.Correo = Imparte.CorreoProfesor
-			JOIN Grupo ON Grupo.SiglaCurso = Imparte.SiglaCurso AND Grupo.NumGrupo = Imparte.NumGrupo AND Grupo.Semestre = Imparte.Semestre AND Grupo.Anno = Imparte.Anno
-			WHERE Profesor.Correo = @CorreoProfesor
+			SELECT * FROM Estudiante e JOIN Matriculado_en me ON me.CorreoEstudiante = e.Correo
+										JOIN Imparte im on im.SiglaCurso = me.SiglaCurso
+										JOIN Profesor p ON p.Correo = im.CorreoProfesor
+										JOIN Grupo g ON g.SiglaCurso = im.SiglaCurso AND g.NumGrupo = im.NumGrupo AND g.Semestre = im.Semestre AND g.Anno = im.Anno
+			WHERE p.Correo = @CorreoProfesor
 
 	--UA-Enfasis
 	IF (@CodigoUnidadAcademica IS NOT NULL AND @CodigoCarrera IS NOT NULL AND @CodigoEnfasis IS NOT NULL AND @SiglaCurso IS NULL AND @NumGrupo IS NULL AND	@Semestre IS NULL AND @Anno IS NULL AND @CorreoProfesor IS NULL )
-			SELECT Grupo.SiglaCurso, Grupo.NumGrupo, Grupo.Semestre, Grupo.Anno
-			FROM UnidadAcademica JOIN Inscrita_en ON  UnidadAcademica.Codigo = Inscrita_en.CodUnidadAc 
-								 JOIN Carrera ON Carrera.Codigo = Inscrita_en.CodCarrera 
-								 JOIN Enfasis ON Enfasis.CodCarrera = Carrera.Codigo
-								 JOIN Pertenece_a ON Pertenece_a.CodCarrera = Carrera.Codigo AND Pertenece_a.CodEnfasis = Enfasis.Codigo
-								 JOIN Curso ON Curso.Sigla = Pertenece_a.SiglaCurso
-								 JOIN Grupo ON Grupo.SiglaCurso = Curso.Sigla
-			WHERE UnidadAcademica.Codigo = @CodigoUnidadAcademica AND Carrera.Codigo = @CodigoCarrera AND Enfasis.Codigo = @CodigoEnfasis
+			SELECT * FROM Estudiante e JOIN Matriculado_en me ON me.CorreoEstudiante = e.Correo
+									JOIN UnidadAcademica ua on ua.Codigo = @CodigoCarrera
+									JOIN Inscrita_en ie ON ua.Codigo = ie.CodUnidadAc 
+									JOIN Carrera ca ON ca.Codigo = ie.CodCarrera 
+									JOIN Enfasis en ON en.CodCarrera = ca.Codigo
+									JOIN Pertenece_a pa ON pa.CodCarrera = ca.Codigo AND pa.CodEnfasis = en.Codigo
+									JOIN Curso c ON c.Sigla = pa.SiglaCurso AND me.SiglaCurso = c.Sigla
+									JOIN Grupo g ON g.SiglaCurso = c.Sigla AND g.NumGrupo = me.NumGrupo
+			WHERE ua.Codigo = @CodigoUnidadAcademica AND ca.Codigo = @CodigoCarrera AND en.Codigo = @CodigoEnfasis
 
 	-- UA-Profesor
 	IF (@CodigoUnidadAcademica IS NOT NULL AND @CodigoCarrera IS  NULL AND @CodigoEnfasis IS  NULL AND @SiglaCurso IS NULL AND @NumGrupo IS NULL AND	@Semestre IS NULL AND @Anno IS NULL AND @CorreoProfesor IS NOT NULL)
-			SELECT Grupo.SiglaCurso, Grupo.NumGrupo, Grupo.Semestre, Grupo.Anno
-			FROM UnidadAcademica JOIN Inscrita_en ON  UnidadAcademica.Codigo = Inscrita_en.CodUnidadAc 
-								 JOIN Carrera ON Carrera.Codigo = Inscrita_en.CodCarrera 
-								 JOIN Enfasis ON Enfasis.CodCarrera = Carrera.Codigo
-								 JOIN Pertenece_a ON Pertenece_a.CodCarrera = Carrera.Codigo AND Pertenece_a.CodEnfasis = Enfasis.Codigo
-								 JOIN Curso ON Curso.Sigla = Pertenece_a.SiglaCurso
-								 JOIN Grupo ON Grupo.SiglaCurso = Curso.Sigla
-								 JOIN Imparte ON  Imparte.NumGrupo = Grupo.NumGrupo AND Imparte.Semestre = Grupo.Semestre AND Grupo.Anno = Imparte.Anno AND Imparte.SiglaCurso = Grupo.SiglaCurso
-								 JOIN Profesor ON Profesor.Correo = Imparte.CorreoProfesor
-			WHERE Profesor.Correo = @CorreoProfesor AND UnidadAcademica.Codigo =  @CodigoUnidadAcademica
+			SELECT * FROM Estudiante e JOIN Matriculado_en me ON me.CorreoEstudiante = e.Correo
+									JOIN UnidadAcademica ua on ua.Codigo = @CodigoCarrera
+									JOIN Inscrita_en ie ON ua.Codigo = ie.CodUnidadAc 
+									JOIN Carrera ca ON ca.Codigo = ie.CodCarrera 
+									JOIN Enfasis en ON en.CodCarrera = ca.Codigo
+									JOIN Pertenece_a pa ON pa.CodCarrera = ca.Codigo AND pa.CodEnfasis = en.Codigo
+									JOIN Curso c ON c.Sigla = pa.SiglaCurso AND me.SiglaCurso = c.Sigla
+									JOIN Grupo g ON g.SiglaCurso = c.Sigla AND g.NumGrupo = me.NumGrupo
+									JOIN Imparte im on im.SiglaCurso = me.SiglaCurso
+									JOIN Profesor p ON im.CorreoProfesor = p.Correo
+			WHERE p.Correo = @CorreoProfesor AND ua.Codigo =  @CodigoUnidadAcademica
 
 	-- Carrera-Enfasis-Profesor
 	IF  (@CodigoUnidadAcademica IS NULL AND @CodigoCarrera IS NOT NULL AND @CodigoEnfasis IS NOT NULL AND @SiglaCurso IS NULL AND @NumGrupo IS NULL AND	@Semestre IS NULL AND @Anno IS NULL AND @CorreoProfesor IS NOT NULL)
-		
-			SELECT Grupo.SiglaCurso, Grupo.NumGrupo, Grupo.Semestre, Grupo.Anno
-			FROM UnidadAcademica JOIN Inscrita_en ON  UnidadAcademica.Codigo = Inscrita_en.CodUnidadAc 
-								 JOIN Carrera ON Carrera.Codigo = Inscrita_en.CodCarrera 
-								 JOIN Enfasis ON Enfasis.CodCarrera = Carrera.Codigo
-								 JOIN Pertenece_a ON Pertenece_a.CodCarrera = Carrera.Codigo AND Pertenece_a.CodEnfasis = Enfasis.Codigo
-								 JOIN Curso ON Curso.Sigla = Pertenece_a.SiglaCurso
-								 JOIN Grupo ON Grupo.SiglaCurso = Curso.Sigla
-								 JOIN Imparte ON  Imparte.NumGrupo = Grupo.NumGrupo AND Imparte.Semestre = Grupo.Semestre AND Grupo.Anno = Imparte.Anno AND Imparte.SiglaCurso = Grupo.SiglaCurso
-								 JOIN Profesor ON Profesor.Correo = Imparte.CorreoProfesor
-			WHERE Carrera.Codigo = @CodigoCarrera	AND Enfasis.Codigo = @CodigoEnfasis AND Profesor.Correo = @CorreoProfesor
+			SELECT * FROM Estudiante e JOIN Matriculado_en me ON me.CorreoEstudiante = e.Correo
+									JOIN UnidadAcademica ua on ua.Codigo = @CodigoCarrera
+									JOIN Inscrita_en ie ON ua.Codigo = ie.CodUnidadAc 
+									JOIN Carrera ca ON ca.Codigo = ie.CodCarrera 
+									JOIN Enfasis en ON en.CodCarrera = ca.Codigo
+									JOIN Pertenece_a pa ON pa.CodCarrera = ca.Codigo AND pa.CodEnfasis = en.Codigo
+									JOIN Curso c ON c.Sigla = pa.SiglaCurso AND me.SiglaCurso = c.Sigla
+									JOIN Grupo g ON g.SiglaCurso = c.Sigla AND g.NumGrupo = me.NumGrupo
+									JOIN Imparte im on im.SiglaCurso = me.SiglaCurso
+									JOIN Profesor p ON im.CorreoProfesor = p.Correo
+			WHERE ca.Codigo = @CodigoCarrera AND en.Codigo = @CodigoEnfasis AND p.Correo = @CorreoProfesor
 
 	-- Todos los que vengan con un grupo
 	IF ( @SiglaCurso IS NOT NULL AND @NumGrupo IS NOT NULL AND	@Semestre IS NOT NULL AND @Anno IS NOT NULL )
-			SELECT Grupo.SiglaCurso, Grupo.NumGrupo, Grupo.Semestre, Grupo.Anno
-			FROM UnidadAcademica JOIN Inscrita_en ON  UnidadAcademica.Codigo = Inscrita_en.CodUnidadAc 
-								 JOIN Carrera ON Carrera.Codigo = Inscrita_en.CodCarrera 
-								 JOIN Enfasis ON Enfasis.CodCarrera = Carrera.Codigo
-								 JOIN Pertenece_a ON Pertenece_a.CodCarrera = Carrera.Codigo AND Pertenece_a.CodEnfasis = Enfasis.Codigo
-								 JOIN Curso ON Curso.Sigla = Pertenece_a.SiglaCurso
-								 JOIN Grupo ON Grupo.SiglaCurso = Curso.Sigla
-			WHERE Grupo.Anno = @Anno AND Grupo.SiglaCurso = @SiglaCurso AND Grupo.Semestre = @Semestre
-RETURN 0*/
+		SELECT * FROM Estudiante e JOIN Matriculado_en me ON me.CorreoEstudiante = e.Correo
+									JOIN UnidadAcademica ua ON ua.Codigo IS NOT NULL 
+									JOIN Inscrita_en ie ON ua.Codigo = ie.CodUnidadAc 
+									JOIN Carrera ca ON ca.Codigo = ie.CodCarrera 
+									JOIN Enfasis en ON en.CodCarrera = ca.Codigo
+									JOIN Pertenece_a pa ON pa.CodCarrera = ca.Codigo AND pa.CodEnfasis = en.Codigo
+									JOIN Curso C ON c.Sigla = pa.SiglaCurso AND me.SiglaCurso = c.Sigla
+									JOIN Grupo g ON g.SiglaCurso = c.Sigla AND g.NumGrupo = me.NumGrupo
+		WHERE g.Anno = @Anno AND g.SiglaCurso = @SiglaCurso AND g.Semestre = @Semestre
+RETURN 0
