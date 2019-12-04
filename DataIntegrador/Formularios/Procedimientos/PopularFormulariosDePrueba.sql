1f255a0be903f247bfe9796f71db5b86f0f5da9c
Gabo141298 <gabriel.galvez@ucr.ac.cr>
2019-12-03 22:31:49 -0600

    Correcciones en el post deployment script y en la prueba de UI.

--- DataIntegrador/Formularios/Procedimientos/PopularFormulariosDePrueba.sql
+++ DataIntegrador/Formularios/Procedimientos/PopularFormulariosDePrueba.sql
@@ -32,20 +32,12 @@ BEGIN
 
 
 			-- Asigna las secciones a este formulario de prueba
-			MERGE INTO Formulario_tiene_seccion AS Target
-			USING (VALUES
-				('FORMSU01', 'INFOPROF', 0),
-				('FORMSU01', 'SUPRUEBA', 1),
-				('CI0128G1', 'CI0128S1', 0),
-				('CI0128G1', 'CI0128S2', 1),
-				('CI0128G1', 'CI0128S3', 2),
-				('CI0128G1', 'CI0128S4', 3)
-			)
-			AS SOURCE ([FCodigo],[SCodigo],[Orden])
-			ON Target.FCodigo = Source.FCodigo and Target.SCodigo = Source.SCodigo
-			WHEN NOT MATCHED BY TARGET THEN
-			INSERT (FCodigo,SCodigo,Orden)
-			VALUES (FCodigo,SCodigo,Orden);
+			EXEC AsociarSeccionConFormulario @CodigoFormulario = 'FORMSU01', @CodigoSeccion = 'INFOPROF';
+			EXEC AsociarSeccionConFormulario @CodigoFormulario = 'FORMSU01', @CodigoSeccion = 'SUPRUEBA';
+			EXEC AsociarSeccionConFormulario @CodigoFormulario = 'CI0128G1', @CodigoSeccion = 'CI0128S1';
+			EXEC AsociarSeccionConFormulario @CodigoFormulario = 'CI0128G1', @CodigoSeccion = 'CI0128S2';
+			EXEC AsociarSeccionConFormulario @CodigoFormulario = 'CI0128G1', @CodigoSeccion = 'CI0128S3';
+			EXEC AsociarSeccionConFormulario @CodigoFormulario = 'CI0128G1', @CodigoSeccion = 'CI0128S4';
 		
 		COMMIT TRANSACTION popularFormulariosPrueba
 	END TRY
