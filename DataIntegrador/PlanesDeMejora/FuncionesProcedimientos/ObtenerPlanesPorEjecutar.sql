f54e152a07082e7a7ecd7d453eecd18940643b6d
Lucho Ramírez <luchoestebanra@gmail.com>
2019-12-16 22:15:57 -0600

    Planes Por Ejecutar

---
+++ DataIntegrador/PlanesDeMejora/FuncionesProcedimientos/ObtenerPlanesPorEjecutar.sql
@@ -0,0 +1,18 @@
+﻿-- Procedimiento para sacar los planes que debe ejecutar un profesor.
+
+-- Ejemplo de uso: 
+-- EXEC dbo.ObtenerPlanesPorEjecutar @correoProf = 'cristian.asch@mail.com'
+
+CREATE PROCEDURE [dbo].[ObtenerPlanesPorEjecutar]
+	@correoProf VARCHAR(50)
+AS
+BEGIN
+	IF(@correoProf IS NOT NULL)
+	BEGIN
+		-- Conseguir los planes basado en el correo del profesor
+		SELECT a.codPlan
+		FROM AsignadoA a
+		JOIN PlanDeMejora p ON p.codigo = a.codPlan
+		WHERE a.corrProf = @correoProf AND p.borrado = 0
+	END
+END
